6736d4a8cd5c85d719ca7e2af1e836bbbc240567
brynnz22 <brynn.zalmanek@sagebase.org>
2022-09-13 08:50:09 -0700

    change double back to str

--- annotations/manifest_upload.py
+++ annotations/manifest_upload.py
@@ -46,7 +46,7 @@ def col_data_type_dict(syn, table_id):
         'INTEGER': int,
         'LARGETEXT': str,
         'STRING_LIST': list,
-        'DOUBLE': float,
+        'DOUBLE': str,
         'LINK': str
     }
     col_types_dict = {k: data_type_dict.get(v, v) for k, v in col_dict.items()}
