cc654f2fd6e0dbb77c3339b1df481f6a439b4859
brynnz22 <brynn.zalmanek@sagebase.org>
2022-11-09 08:59:22 -0800

    fix indenting in for loop

--- annotations/manifest_upload.py
+++ annotations/manifest_upload.py
@@ -74,10 +74,8 @@ def edit_manifest(file_path, col_types_dict):
             df[columnName] = df[columnName].astype(str)
             df[columnName] = df[columnName].str.split(', ')
         else:
-            df[columnName] = df[columnName].astype(
-                col_types_dict[columnName])
-                df[columnName] = df[columnName].astype(
-                    col_types_dict[columnName])
+            df[columnName] = df[columnName].astype(col_types_dict[columnName])
+            df[columnName] = df[columnName].astype(col_types_dict[columnName])
             # For columns with USERID as datatype, remove .0 tacked on in data type conversion.
             if col_types_dict.get(columnName) == 'USERID':
                 df[columnName] = df[columnName].replace("\.0$", "")
