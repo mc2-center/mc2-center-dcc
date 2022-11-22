194bde402dad5d28b33754d672ca6a9361ddbfcf
Victor Baham <bahamvictor75@gmail.com>
2022-11-21 18:31:48 -0800

    revised code to accommodate all code review suggestions

--- portal_tables/unify_grant_tables.py
+++ portal_tables/unify_grant_tables.py
@@ -5,13 +5,12 @@ This script assumes the folders for PublicationView,
 DatasetView, and ToolView, respectively, remain 
 consistent in the CCKP admin project table.
 
+command line arguments: 1. Portal - Grants Merged Synapse ID 2. CCKP Admin Project Synapse ID
+test value for argument 1: syn42801895
+test value for argument 2: syn35629947 
+
 author: victor.baham
 """
-
-# command line arguments: 1. Portal - Grants Merged Synapse ID 2. CCKP Admin Project Synapse ID
-# test value for argument 1: syn42801895
-# test value for argument 2: syn35629947 
-
 import argparse
 import synapseclient
 from synapseclient import Project, File, Folder
@@ -20,124 +19,95 @@ import itertools
 import pandas as pd
 
 def get_args():
-    #Retrieve Synapse table IDs from command line
+    """This function gets the Synapse tables to be 
+    processed from command line arguments."""
     parser = argparse.ArgumentParser(
         description="Merge publications, datasets, and tools from Grants Merged table into CCKP admin table")
     parser.add_argument("-p1", "--project1",
-                        type=str, required=True,
+                        type=str, default="syn42801895",
                         help="Portal - Grants Merged Synapse ID.")
     parser.add_argument("-p2", "--project2",
-                        type=str, required=True,
+                        type=str, default="syn35629947",
                         help=("CCKP - MC2 Admin Synapse ID"))
     parser.add_argument("--dryrun", action="store_true")
     return parser.parse_args()
 
-def getManifestsFromGrant(syn,theParent):
+def get_manifests_from_grant(syn, the_parent):
     """This function examines the GrantView table
     and returns a list of all Synapse table IDs
     separated by manifest type (publications, datasets,
-    tools) for each grant"""
+    tools) for each grant."""
 
-    # Access each grant project by accessing 'grantId'
-    # column in grantView manifest
-    # synId for grantView is hardcoded in and
-    # will need to be changed for future use
-    df = syn.tableQuery(f"select {'grantId'} from {theParent}").asDataFrame()
-    gIds = df['grantId'].tolist()
+    df = syn.tableQuery(f"select {'grantId'} from {the_parent}").asDataFrame()
+    g_Ids = df['grantId'].tolist()
 
-    # for each grant, get publications,
-    # datasets, and tools 
+    m_Types = []
+    m_Ids = []
 
-    # value inside dictionary will reset upon each
-    # loop iteration, so appending to this list 
-    # maintains each one
-    mTypes = []
-    mIds = []
-    for gId in gIds:
-        grantX = syn.get(gId)
-        # By accessing the grants, it is possible
-        # to access their 'children'
+    list_to_flatten = []
+    
+    for g_Id in g_Ids:
+        grant_X = syn.get(g_Id)
+        id_to_manifest_types = {
+            m.get('id'): m.get('name') 
+            for m in syn.getChildren(grant_X, includeTypes=['table'])
+        }
+        list_to_flatten.append(id_to_manifest_types)
+    
+    id_to_man_final = {k:v for d in list_to_flatten for k,v in d.items()}
 
-        # extract manifest type and table id from generator
-        mTypes.append([m['name'] for m in syn.getChildren(grantX, includeTypes=['table'])])
-        mIds.append([m['id'] for m in syn.getChildren(grantX, includeTypes=['table'])])
+    pubs_Id = []
+    datasets_Id = []
+    tools_Id = []
 
-    # with each synapse table id mapped to a type of manifest
-    # it is possible to begin adding them to the staging table
-    # but should flatten list first for easier key-val mapping
-    flat_mTypes = list(itertools.chain(*mTypes))
-    flat_mIds = list(itertools.chain(*mIds))
+    for key,val in id_to_man_final.items():
+        if 'publication' in val.lower():
+            pubs_Id.append(key)
+        elif 'dataset' in val.lower():
+            datasets_Id.append(key)
+        elif 'tool' in val.lower():
+            tools_Id.append(key)
+    
+    return pubs_Id, datasets_Id, tools_Id
 
-    idToManType = dict(zip(flat_mIds, flat_mTypes))
-    print(idToManType)
-
-    # use dictionary of synapse id to manifest type
-    # to prepare to separate data into rows
-    pubsId = []
-    datasetsId = []
-    toolsId = []
-
-    for key,val in idToManType.items():
-        if val == 'publications':
-            pubsId.append(key)
-        elif val == 'datasets':
-            datasetsId.append(key)
-        else:
-            toolsId.append(key)
-    return pubsId, datasetsId, toolsId
-
-def getEachManifestFromCCKP(syn, theNewParent):
-    """this function reads in the Synapse ID of the
+def get_each_manifest_from_CCKP(syn, the_new_parent):
+    """This function reads in the Synapse ID of the
     CCKP Admin table and finds the table IDs of the
-    PublicationView, DatasetView, and ToolView manifests"""
+    PublicationView, DatasetView, and ToolView manifests."""
 
-    pMan = syn.findEntityId('PublicationView', theNewParent)
-    dMan = syn.findEntityId('DatasetView', theNewParent)
-    tMan = syn.findEntityId('ToolView', theNewParent)
+    p_Man = syn.findEntityId('PublicationView', the_new_parent)
+    d_Man = syn.findEntityId('DatasetView', the_new_parent)
+    t_Man = syn.findEntityId('ToolView', the_new_parent)
 
-    return pMan, dMan, tMan
+    return p_Man, d_Man, t_Man
+
+def write_manifest_to_CCKP(syn, manifests_by_type, table_id):
+    """This function merges all manifests of the same type
+    from each grant project into one DataFrame and appends 
+    the collective rows to the CCKP admin table.
+    """
+    man_dfs = []
+    for man in manifests_by_type:
+        man_df = syn.tableQuery(f"select * from {man}").asDataFrame()
+        man_dfs.append(man_df)
+    final_mans = pd.concat(man_dfs, ignore_index=True)
+    syn.store(Table(table_id, final_mans))
 
 def main():
     syn = synapseclient.Synapse()
     syn.login()
     
     args = get_args() 
-    originTable = args.project1
-    destinationTable = args.project2
+    origin_table = args.project1
+    destination_table = args.project2
 
-    # these are the lists of each kind of manifest pulled from
-    # all individual grants
-    [allPub, allData, allTool] = getManifestsFromGrant(syn, originTable)
-    # these are the table IDs of the PublicationView, DataView, and ToolView
-    # manifests in the CCKP Admin Project table
-    [synPub, synData, synTool] = getEachManifestFromCCKP(syn, destinationTable)
+    all_Pub, all_Data, all_Tool = get_manifests_from_grant(syn, origin_table)
+
+    syn_Pub, syn_Data, syn_Tool = get_each_manifest_from_CCKP(syn, destination_table)
     
-    # create merged DataFrame and write to
-    # PublicationView manifest in CCKP
-    pub_dfs = []
-    for pub in allPub:
-        pub_df = syn.tableQuery(f"select * from {pub}").asDataFrame()
-        pub_dfs.append(pub_df) 
-    final_pubs = pd.concat(pub_dfs, ignore_index=True)
-    syn.store(Table(synPub, final_pubs))
-
-    # create merged DataFrame and write to
-    # DatasetView manifest in CCKP
-    dataset_dfs = []
-    for dataset in allData:
-        dataset_df = syn.tableQuery(f"select * from {dataset}").asDataFrame()
-        dataset_dfs.append(dataset_df)
-    final_datasets = pd.concat(dataset_dfs, ignore_index=True)   
-    syn.store(Table(synData, final_datasets))
-
-    # create merged DataFrame and write to
-    # ToolView manifest in CCKP
-    tool_dfs = []
-    for tool in allTool:
-        tool_df = syn.tableQuery(f"select * from {tool}").asDataFrame()
-        tool_dfs.append(tool_df)
-    final_tools = pd.concat(tool_dfs, ignore_index=True)
-    syn.store(Table(synTool, final_tools))
+    write_manifest_to_CCKP(syn, all_Pub, syn_Pub)
+    write_manifest_to_CCKP(syn, all_Data, syn_Data)
+    write_manifest_to_CCKP(syn, all_Tool, syn_Tool)
 
     print('The tables have been successfully merged into the CCKP Admin project.')
 
