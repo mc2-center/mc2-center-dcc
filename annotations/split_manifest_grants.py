15ce32fa3c44223319449b37e98520fdf29136e2
brynnz22 <46435419+brynnz22@users.noreply.github.com>
2023-03-03 14:29:37 -0800

    add split manifest script for csvs (#22)
    * add split manifest script for csvs
    
    * Remove leading space in grant numbers
    
    Added list comprehension that removes leading space from grant numbers. Addresses issue where multiple CSVs were generated for a single grant number because it was listed with other grant numbers in the "Publication Grant Number" column
    
    * add space in code and delete extra line
    
    * add fixes
    
    * remove df conversion function
    
    ---------
    
    Co-authored-by: Orion Banks <49208907+Bankso@users.noreply.github.com>

---
+++ annotations/split_manifest_grants.py
@@ -0,0 +1,57 @@
+"""Split Manifests CSV
+This script will split a manifest csv by grant number and output
+results into individual CSVs.
+author: verena.chung
+author: brynn.zalmanek
+"""
+
+import os
+import argparse
+import pandas as pd
+
+
+def get_args():
+    """Set up command-line interface and get arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("manifest",
+                        type=str,
+                        help="path of manifest to be split")
+    parser.add_argument("manifest_type",
+                        type=str,
+                        choices=["publication", "dataset", "tool", "project"],
+                        help="type of manifest to split, e.g. publicaiton")
+    parser.add_argument("folder",
+                        type=str,
+                        help="folder path to save split manifests in")
+    return parser.parse_args()
+
+
+def split_manifest(df, manifest_type, directory):
+    """Split manifest into multiple manifests by grant number"""
+    colname = f"{manifest_type.capitalize()} Grant Number"
+
+    df[colname] = df[colname].str.split(", ")
+
+    grouped = df.explode(colname).groupby(colname)
+    print(f"Found {len(grouped.groups)} grant numbers in table "
+          "- splitting now...")
+
+    for grant_number in grouped.groups:
+        df = grouped.get_group(grant_number)
+        df.to_csv(f"{directory}/{grant_number}_{type}.csv", index=False)
+
+
+def main():
+    """Main function."""
+
+    args = get_args()
+
+    df = pd.read_csv(args.manifest)
+
+    split_manifest(df, args.manifest_type, args.folder)
+
+    print("manifests split!")
+
+
+if __name__ == "__main__":
+    main()
