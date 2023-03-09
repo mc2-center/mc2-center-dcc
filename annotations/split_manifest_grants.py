719e49f9ee3787fc0234f0769aeb7ea93f8c0186
Verena Chung <9377970+vpchung@users.noreply.github.com>
2023-03-08 16:09:26 -0800

    Output split manifests as Excel files (#25)
    * output manifests as excel files
    
    * add another sheet of standard terms
    
    * add requirements.txt
    
    * update docstrings
    
    * update source for cv terms
    
    * remove dependency from requirements
    
    * standardize format

--- annotations/split_manifest_grants.py
+++ annotations/split_manifest_grants.py
@@ -1,13 +1,17 @@
 """Split Manifests CSV
 This script will split a manifest csv by grant number and output
-results into individual CSVs.
+results into individual Excel files.
 author: verena.chung
 author: brynn.zalmanek
 """
 
 import os
 import argparse
+
 import pandas as pd
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import Font
 
 
 def get_args():
@@ -26,8 +30,32 @@ def get_args():
     return parser.parse_args()
 
 
-def split_manifest(df, manifest_type, directory):
-    """Split manifest into multiple manifests by grant number"""
+def generate_manifest_as_excel(df, cv_terms, output):
+    """Generate manifest file (xlsx) with given df."""
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "manifest"
+    for r in dataframe_to_rows(df, index=False, header=True):
+        ws.append(r)
+
+    ws2 = wb.create_sheet("standard_terms")
+    for row in dataframe_to_rows(cv_terms, index=False, header=True):
+        ws2.append(row)
+
+    # Style the worksheet.
+    ft = Font(bold=True)
+    ws2['A1'].font = ft
+    ws2['B1'].font = ft
+    ws2['C1'].font = ft
+    ws2.column_dimensions['A'].width = 18
+    ws2.column_dimensions['B'].width = 60
+    ws2.column_dimensions['C'].width = 12
+    ws2.protection.sheet = True
+    wb.save(output)
+
+
+def split_manifest(df, manifest_type):
+    """Split manifest into multiple manifests by grant number."""
     colname = f"{manifest_type.capitalize()} Grant Number"
 
     df[colname] = df[colname].str.split(", ")
@@ -35,10 +63,7 @@ def split_manifest(df, manifest_type, directory):
     grouped = df.explode(colname).groupby(colname)
     print(f"Found {len(grouped.groups)} grant numbers in table "
           "- splitting now...")
-
-    for grant_number in grouped.groups:
-        df = grouped.get_group(grant_number)
-        df.to_csv(f"{directory}/{grant_number}_{type}.csv", index=False)
+    return grouped
 
 
 def main():
@@ -46,10 +71,30 @@ def main():
 
     args = get_args()
 
-    df = pd.read_csv(args.manifest)
+    # Create output directory if it does not already exist.
+    output_dir = args.folder
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    split_manifest(df, args.manifest_type, args.folder)
+    # Get latest CV terms to save as "standard_terms" - only keeping
+    # the terms relevant to the manifest type.
+    manifest_type = args.manifest_type
+    annots = ["assay", "tissue", "tumorType"]
 
+    cv_file = "https://raw.githubusercontent.com/mc2-center/data-models/main/all_valid_values.csv"
+    cv_terms = pd.read_csv(cv_file)
+    cv_terms = cv_terms.loc[cv_terms['category'].str.contains(manifest_type) |
+                            cv_terms['category'].isin(annots)]
+
+    # Read in manifest then split by grant number.  For each grant, generate a new
+    # manifest as an Excel file.
+    manifest = pd.read_csv(args.manifest)
+    split_manifests = split_manifest(manifest, manifest_type)
+    for grant_number in split_manifests.groups:
+        df = split_manifests.get_group(grant_number)
+        path = os.path.join(
+            output_dir, f"{grant_number}_{manifest_type}.xlsx")
+        generate_manifest_as_excel(df, cv_terms, path)
     print("manifests split!")
 
 
