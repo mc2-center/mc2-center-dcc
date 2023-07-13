6d10864a8733dc67f3dffb91c0442874df73e114
Orion Banks <49208907+Bankso@users.noreply.github.com>
2023-07-13 14:55:54 -0700

    Create get_entity_ids.py
    Adds a script to gather Synapse IDs for entities with a name that matches an input string. Matches are searched for within each entity contained in a list of Synapse IDs provided at runtime.
    
    Design
    - searches through a list of Synapse project IDs (though any Synapse ID could work) provided as '-l'
    - identifies Synapse objects that match the input string provided as '-n'
    -outputs a csv containing the input Synapse ID and the Synapse ID for the entity with a name that matches the input string
    
    Primary use case is to recover Synapse IDs for all 'publications' folders contained in Synapse projects that were previously created for each grant supported by the MC2 Center, based on the Synapse IDs of the grant-based projects.

---
+++ utils/get_entity_ids.py
@@ -0,0 +1,51 @@
+import synapseclient
+import argparse
+import pandas as pd
+from pathlib import Path
+
+### Login to Synapse ###
+def login():
+
+    syn = synapseclient.Synapse()
+    syn.login()
+
+    return syn
+
+
+def get_args():
+	
+	parser = argparse.ArgumentParser(
+        description='Get synapse project folder ids')
+	parser.add_argument('-l',
+						nargs='+',
+                        help='Synapse project IDs to query.')
+	parser.add_argument('-n',
+                        type=str,
+                        help='Entity name to search for in Synapse project.')
+	return parser.parse_args()
+
+
+def get_list(syn, name, projects):
+	
+	df = pd.DataFrame(columns=['projectId', 'entityId'])
+
+	for project in projects:
+		entityId = syn.findEntityId(name, project)
+		newRow = pd.DataFrame([[project, entityId]], columns=['projectId', 'entityId'])
+		df = pd.concat([df, newRow])
+	
+	df = df.reset_index(drop=True)
+	return df
+
+def main():
+	
+	syn = login()
+	args = get_args()
+	out = get_list(syn, args.n, args.l)
+
+	outPath = Path('output/project_entity.csv')
+	outPath.parent.mkdir(parents=True, exist_ok=True)
+	out.to_csv(outPath, index=True)
+
+if __name__ == "__main__":
+    main()
