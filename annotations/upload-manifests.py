f82c2d4d91150d59dca16d817b978c547287d008
Orion Banks <49208907+Bankso@users.noreply.github.com>
2024-01-24 12:40:35 -0800

    Updates and additions from backpopulation work (#33)
    * Create get_entity_ids.py
    
    Adds a script to gather Synapse IDs for entities with a name that matches an input string. Matches are searched for within each entity contained in a list of Synapse IDs provided at runtime.
    
    Design
    - searches through a list of Synapse project IDs (though any Synapse ID could work) provided as '-l'
    - identifies Synapse objects that match the input string provided as '-n'
    -outputs a csv containing the input Synapse ID and the Synapse ID for the entity with a name that matches the input string
    
    Primary use case is to recover Synapse IDs for all 'publications' folders contained in Synapse projects that were previously created for each grant supported by the MC2 Center, based on the Synapse IDs of the grant-based projects.
    
    * Update .gitignore
    
    Added 'output/', so results from get_entity_ids.py are ignored
    
    * Add CSV generator to split_manifest_grants.py
    
    Add flag to designate if CSV output is desired ("--csv")
    Add logic to main function that generates CSV from split manifests when "--csv" flag is passed at command line
    
    * Create merge_tables.py
    
    Addition of a script that combines tables stored in Synapse to create a MaterializedView table
    
    * Add modified back population script
    
    * Delete bda1b8c8c8304d315cdf04babeb48c4c.json
    
    * Add script to check Synapse cert status
    
    * Update upload-manifests.py
    
    * Create build_table_view.py
    
    Adds a basic script for creating an entity view table, based on command line inputs
    
    * Update merge_tables.py
    
    Implement entity view query to get table synapse IDs for materialized view SQL
    
    * Create create_id_folders.py
    
    Adds a functioning script that creates Synapse folders for manifest entries, captures the Synapse IDs, and populates the <component>_id column of each manifest with the Synapse IDs.
    
    Script takes the upload manifest CSV (file_path, target_id; used by upload-scripts.py) as input and overwrites the <component>_id column of manifests found at the end of each path.
    
    The unique IDs will serve as primary keys and will enable table updates to be made via schematic 'upsert'
    
    * Create upload_files.py
    
    Adds a script for bulk upload of resources to Synapse, using the 'syncToSynapse' function in the 'synapseutils' package
    
    * Update .gitignore
    
    * Update split_manifest_grants.py
    
    * Create make_folders.py
    
    * Update publications-status-check.yml
    
    * Update upload_files.py
    
    -added description of TSV input
    
    * Update build_table_view.py
    
    - added description to script
    
    * Update create_id_folders.py
    
    - added description of script

---
+++ annotations/upload-manifests.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import synapseclient
+import multiprocessing
+import subprocess
+import sys
+import argparse
+from functools import partial
+
+'''
+
+This script accepts a CSV file containing manifest file paths and their corresponding Synapse IDs for target folders. 
+It then performs a parallel validation check, after which it parallel uploads the manifests to Synapse
+
+'''
+def get_args():
+    """Set up command-line interface and get arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m",
+                        type=str,
+                        help="path to CSV containing file paths and target folder synIDs")
+    parser.add_argument("-t",
+                        type=str,
+                        choices=["PublicationView", "DatasetView", "ToolView", "EducationalResource"],
+                        help="type of manifest(s) being validated and uploaded")
+    parser.add_argument("-c",
+                        type=str,
+                        help="path to a schematic config file")
+    parser.add_argument("-v",
+                        action="store_false",
+                        help="Boolean; if this flag is provided, invalid manifests will be submitted")
+    return parser.parse_args()
+
+def login():
+    """Login to Synapse"""
+    syn = synapseclient.Synapse()
+    syn.login()
+    return syn
+
+def validate_entry_worker(fp, cf, mt, valid_only):
+    print(f"Validating file: {fp} of type {mt}")
+    validate_command = [
+        "schematic",
+        "model",
+        "-c",
+        cf,  
+        "validate",
+        "-dt",
+        mt,
+        "-mp",
+        fp
+    ]
+    
+    print(f"Running validation command: {' '.join(validate_command)}")
+    try:
+        subprocess.run(validate_command, check=True, stdout=sys.stdout, stderr=subprocess.STDOUT)
+        return fp  # Validation succeeded, return the filepath
+    except subprocess.CalledProcessError:
+        print(f"File with {fp} could not be validated.") # Validation failed
+        if valid_only == True:
+            return None
+        elif valid_only == False:
+            return fp
+
+def submit_entry_worker(args, cf):
+    fp, target_id = args
+    print(f"Submitting file: {fp} with target ID: {target_id}")
+    command = [
+        "schematic",
+        "model",
+        "-c",
+        cf,  
+        "submit",
+        "-mp",
+        fp,
+        "-d",
+        target_id,
+        "-dl",
+        "-mrt",
+        "table_and_file",
+        "-tm",
+        "upsert"
+    ]
+
+    
+    cmd_line = " ".join(command)
+    
+    print(cmd_line)
+    
+    subprocess.run(command, stdout=sys.stdout, stderr=subprocess.STDOUT)
+
+
+def main():
+    
+    #syn = login()
+    args = get_args()
+    csv_file = args.m
+    config_file = args.c
+    manifest_type = args.t
+    submit_valid = args.v
+
+    df = pd.read_csv(csv_file)
+    pd.set_option('display.max_colwidth', 90)
+
+    num_processes = multiprocessing.cpu_count()  # Number of CPU cores
+    pool = multiprocessing.Pool(processes=num_processes)
+
+    validation_args_list = df['file_path'].tolist() # pull manifest paths from input CSV for validation
+
+    validated_files = pool.map(partial(
+        validate_entry_worker, cf=config_file, mt=manifest_type, valid_only=submit_valid), 
+        validation_args_list)
+
+    pool.close()
+    pool.join()
+
+    validated_files = [fp for fp in validated_files if fp is not None]
+
+    submit_pool = multiprocessing.Pool(processes=num_processes)
+
+    submit_args_list = [(fp, df.loc[i, 'target_id']) for i, fp in enumerate(validated_files)]
+    print(pd.DataFrame(submit_args_list))
+    
+    choice = input(
+        "\n\nReview the printed list of arguments for errors in path and target matches. Type 'upload' to continue or 'end' if you see an error")
+    
+    if choice == 'upload':
+        submit_pool.map(partial(
+            submit_entry_worker, cf=config_file), 
+            submit_args_list)
+
+        submit_pool.close()
+        submit_pool.join()
+    
+    elif choice == 'end':
+        print("\n\nManifests will NOT be uploaded. Exiting now.")
+        exit
+        
+if __name__ == "__main__":
+    main()
+
+
+
+
