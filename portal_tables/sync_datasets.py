34fc420f47eec59eba93967461bc54e0da6ef94f
verena <9377970+vpchung@users.noreply.github.com>
2024-02-20 00:53:58 -0800

    update default values; add two new parameters

--- portal_tables/sync_datasets.py
+++ portal_tables/sync_datasets.py
@@ -15,16 +15,35 @@ import utils
 
 def get_args():
     """Set up command-line interface and get arguments."""
-    parser = argparse.ArgumentParser(
-        description="Add new datasets to the CCKP")
-    parser.add_argument("-m", "--manifest",
-                        type=str, default="syn35492812",
-                        help="Synapse ID to the manifest table/fileview.")
-    parser.add_argument("-t", "--portal_table",
-                        type=str, default="syn21897968",
-                        help=("Add datasets to this specified "
-                              "table. (Default: syn21897968)"))
+    parser = argparse.ArgumentParser(description="Add new datasets to the CCKP")
+    parser.add_argument(
+        "-m",
+        "--manifest_id",
+        type=str,
+        default="syn53478774",
+        help="Synapse ID to the manifest CSV file.",
+    )
+    parser.add_argument(
+        "-t",
+        "--portal_table_id",
+        type=str,
+        default="syn21897968",
+        help="Add datasets to this specified table. (Default: syn21897968)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_csv",
+        type=str,
+        default="./final_dataset_table.csv",
+        help="Filepath to output CSV.",
+    )
     parser.add_argument("--dryrun", action="store_true")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Output all logs and interim tables.",
+    )
     return parser.parse_args()
 
 
