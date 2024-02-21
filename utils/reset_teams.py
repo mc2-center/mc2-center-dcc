8f43338a2a37c3bfbbd7c1738c1d2887e1d86f5b
Verena Chung <9377970+vpchung@users.noreply.github.com>
2024-02-21 11:03:51 -0800

    [fix] Update teams reset script (#46)
    * get list of teams from table
    
    * remove Julie from list of team managers 🥹
    
    * lint; add type hinting

--- utils/reset_teams.py
+++ utils/reset_teams.py
@@ -3,60 +3,42 @@
 This script will remove all team members from a given Synapse team
 ID (-t), with the exception of:
 
-  * Julie Bletz (jbletz, 3361792)
   * Ashley Clayton (ashley.clayton, 3408068)
   * Amber Nelson (ambernelson, 3419821)
   * Verena Chung (vchung, 3393723)
 """
 
-import os
 import argparse
-import getpass
+from typing import List
 
 import synapseclient
-
-
-def login():
-    """Log into Synapse. If env variables not found, prompt user.
-
-    Returns:
-        syn: Synapse object
-    """
-    try:
-        syn = synapseclient.login(
-            os.getenv('SYN_USERNAME'),
-            apiKey=os.getenv('SYN_APIKEY'),
-            silent=True)
-    except synapseclient.core.exceptions.SynapseNoCredentialsError:
-        print("Credentials not found; please manually provide your",
-              "Synapse username and password.")
-        username = input("Synapse username: ")
-        password = getpass.getpass("Synapse password: ")
-        syn = synapseclient.login(username, password, silent=True)
-    return syn
 
 
 def get_args():
     """Set up command-line interface and get arguments."""
     parser = argparse.ArgumentParser(
-        description=("Removes all non-manager team members from a given "
-                     "Synapse team ID."))
-    parser.add_argument("-t", "--team_id",
-                        type=int, required=True,
-                        help="Synapse team ID, e.g. 3424242")
+        description="Removes all non-manager team members from Synapse teams."
+    )
+    parser.add_argument(
+        "-t",
+        "--table_id",
+        type=str,
+        required=True,
+        help="Synapse table containing team IDs to truncate.",
+    )
     return parser.parse_args()
 
 
-def truncate_members(syn, team_id):
+def truncate_members(syn: synapseclient.Synapse, team_id: str) -> None:
     """Remove all non-manager Synapse users from given team."""
 
-    # Synapse user IDs for Julie, Amber, Ashley, and Verena - DO NOT REMOVE FROM TEAM!
-    manager_ids = ["3361792", "3408068", "3419821", "3393723"]
+    # Synapse user IDs for Amber, Ashley, and Verena - DO NOT REMOVE FROM TEAM!
+    manager_ids = ["3408068", "3419821", "3393723"]
 
     count = 0
-    team_members = [m.get('member') for m in syn.getTeamMembers(team_id)]
+    team_members = [m.get("member") for m in syn.getTeamMembers(team_id)]
     for user in team_members:
-        user_id = user.get('ownerId')
+        user_id = user.get("ownerId")
         if user_id not in manager_ids:
             syn.restDELETE(f"/team/{team_id}/member/{user_id}")
             count += 1
@@ -66,12 +48,26 @@ def truncate_members(syn, team_id):
     print(f"Removed {count} members from team: {team.get('name')}")
 
 
+def reset_teams(syn: synapseclient.Synapse, teams: List[str]) -> None:
+    """Reset teams by removing all non-manager members."""
+    for team in teams:
+        truncate_members(syn, team)
+
+
+def get_teams(syn: synapseclient.Synapse, table_id: str) -> List[str]:
+    """Return a list of team IDs."""
+    return (
+        syn.tableQuery(f"SELECT team_id FROM {table_id}").asDataFrame().team_id.tolist()
+    )
+
+
 def main():
     """Main function."""
-    syn = login()
+    syn = synapseclient.login(silent=True)
     args = get_args()
 
-    truncate_members(syn, args.team_id)
+    teams = get_teams(syn, args.table_id)
+    reset_teams(syn, teams)
 
 
 if __name__ == "__main__":
