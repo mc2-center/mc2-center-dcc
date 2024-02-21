a364de603a8ba4ee0dc8176498ac80684631338d
Verena Chung <9377970+vpchung@users.noreply.github.com>
2024-02-21 10:27:57 -0800

    [fix]: Update datasets syncing script (#41)
    * update default values; add two new parameters
    
    * update `sort_and_stringify_col` function
    
    * update colnames used
    
    * comment out folder creation
    
    * account for missing/non standard PMID and grant number
    
    * print out CSV output file
    
    * remove non-breaking space unicode to a regular space
    
    * add FIXME comments
    
    * remove unneeded folder creation function
    
    * improve message about no datasets to sync
    
    * extract common functions to `utils`
    
    * only save one synID for `DatasetView_id`
    
    * remove check for existing datasets
    
    * add type hinting
    
    * replace username/password with authToken
    
    * add log about "syncing" table with latest data
    
    * add step to create a snapshot
    
    * lint

--- portal_tables/sync_datasets.py
+++ portal_tables/sync_datasets.py
@@ -7,100 +7,129 @@ new dataset in its respective grant Project.
 
 import argparse
 
-import re
-from synapseclient import Table, Folder
 import pandas as pd
 import utils
 
 
 def get_args():
     """Set up command-line interface and get arguments."""
-    parser = argparse.ArgumentParser(
-        description="Add new datasets to the CCKP")
-    parser.add_argument("-m", "--manifest",
-                        type=str, default="syn35492812",
-                        help="Synapse ID to the manifest table/fileview.")
-    parser.add_argument("-t", "--portal_table",
-                        type=str, default="syn21897968",
-                        help=("Add datasets to this specified "
-                              "table. (Default: syn21897968)"))
+    parser = argparse.ArgumentParser(description="Add new datasets to the CCKP")
+    parser.add_argument(
+        "-m",
+        "--manifest_id",
+        type=str,
+        default="syn53478774",
+        help="Synapse ID to the manifest CSV file.",
+    )
+    parser.add_argument(
+        "-t",
+        "--portal_table_id",
+        type=str,
+        default="syn21897968",
+        help="Add datasets to this specified table. (Default: syn21897968)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_csv",
+        type=str,
+        default="./final_dataset_table.csv",
+        help="Filepath to output CSV.",
+    )
     parser.add_argument("--dryrun", action="store_true")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Output all logs and interim tables.",
+    )
     return parser.parse_args()
 
 
-def create_folder(syn, name, parent):
-    name = name.replace("/", "-")
-    folder = Folder(name, parent=parent)
-    folder = syn.store(folder)
-    return folder.id
-
-
-def add_missing_info(syn, datasets, grants, pubs):
-    """Add missing information into table before syncing.
-
-    Returns:
-        datasets: Data frame
-    """
-    datasets['link'] = [
-        "".join(["[", d_id, "](", url, ")"])
-        for d_id, url
-        in zip(datasets['datasetAlias'], datasets['datasetUrl'])
+def add_missing_info(
+    datasets: pd.DataFrame, grants: pd.DataFrame, pubs: pd.DataFrame
+) -> pd.DataFrame:
+    """Add missing information into table before syncing."""
+    datasets["link"] = [
+        "".join(["[", d_id, "](", url, ")"]) if url else ""
+        for d_id, url in zip(datasets["DatasetAlias"], datasets["DatasetUrl"])
     ]
-    datasets['grantName'] = ""
-    datasets['themes'] = ""
-    datasets['consortia'] = ""
-    datasets['pub'] = ""
+    datasets["grantName"] = ""
+    datasets["themes"] = ""
+    datasets["consortia"] = ""
+    datasets["pub"] = ""
     for _, row in datasets.iterrows():
-        if re.search(r"^syn\d+$", row['datasetAlias']):
-            folder_id = row['datasetAlias']
-        else:
-            grant_proj = grants[grants.grantNumber ==
-                                row['datasetGrantNumber'][0]]['grantId'].values[0]
-            folder_id = ""
-            folder_id = create_folder(syn, row['datasetAlias'], grant_proj)
-        datasets.at[_, 'id'] = folder_id
         grant_names = []
         themes = set()
         consortia = set()
-        for g in row['datasetGrantNumber']:
-            grant_names.append(grants[grants.grantNumber == g]
-                               ['grantName'].values[0])
-            themes.update(grants[grants.grantNumber == g]
-                          ['theme'].values[0])
-            consortia.update(grants[grants.grantNumber == g]
-                          ['consortium'].values[0])
-        datasets.at[_, 'grantName'] = grant_names
-        datasets.at[_, 'themes'] = list(themes)
-        datasets.at[_, 'consortia'] = list(consortia)
+        for g in row["DatasetGrantNumber"].split(","):
+            if g != "Affiliated/Non-Grant Associated":
+                grant_names.append(
+                    grants[grants.grantNumber == g]["grantName"].values[0]
+                )
+                themes.update(grants[grants.grantNumber == g]["theme"].values[0])
+                consortia.update(
+                    grants[grants.grantNumber == g]["consortium"].values[0]
+                )
+        datasets.at[_, "grantName"] = grant_names
+        datasets.at[_, "themes"] = list(themes)
+        datasets.at[_, "consortia"] = list(consortia)
         pub_titles = []
-        for p in row["datasetPubmedId"]:
-            pub_titles.append(pubs[pubs.pubMedId == int(p)]
-                              ["publicationTitle"].values[0])
+        for p in row["DatasetPubmedId"].split(","):
+            p = p.strip()  # Remove leading/trailing whitespace, if any
+            try:
+                pub_titles.append(
+                    pubs[pubs.pubMedId == int(p)]["publicationTitle"]
+                    .values[0]
+                    .replace("\xa0", " ")
+                )
+            except (ValueError, IndexError):
+                pass  # PMID not yet annotated or found in portal table
         datasets.at[_, "pub"] = pub_titles
     return datasets
 
 
-def sync_table(syn, datasets, table):
-    """Add dataset annotations to the Synapse table."""
-    schema = syn.get(table)
+def clean_table(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean up the table one final time."""
+
+    # Convert string columns to string-list.
+    for col in [
+        "DatasetView_id",
+        "DatasetFileFormats",
+        "DatasetAssay",
+        "DatasetSpecies",
+        "DatasetTissue",
+        "DatasetTumorType",
+        "DatasetGrantNumber",
+        "DatasetPubmedId",
+    ]:
+        df[col] = utils.convert_to_stringlist(df[col])
+
+    # We only need one synID for the portal table. See
+    # https://github.com/mc2-center/mc2-center-dcc/pull/41#issuecomment-1955119623
+    # for more context.
+    df["DatasetView_id"] = df["DatasetView_id"].str[0]
 
     # Reorder columns to match the table order.
     col_order = [
-        'id', 'datasetName', 'datasetAlias', 'datasetDescription',
-        'datasetDesign', 'datasetFileFormats', 'datasetAssay',
-        'datasetSpecies', 'datasetTissue', 'datasetTumorType',
-        'themes', 'consortia', 'datasetGrantNumber',
-        'grantName', 'datasetPubmedId', 'pub', 'link'
+        "DatasetView_id",
+        "DatasetName",
+        "DatasetAlias",
+        "DatasetDescription",
+        "DatasetDesign",
+        "DatasetFileFormats",
+        "DatasetAssay",
+        "DatasetSpecies",
+        "DatasetTissue",
+        "DatasetTumorType",
+        "themes",
+        "consortia",
+        "DatasetGrantNumber",
+        "grantName",
+        "DatasetPubmedId",
+        "pub",
+        "link",
     ]
-    datasets = datasets[col_order]
-
-    new_rows = datasets.values.tolist()
-    syn.store(Table(schema, new_rows))
-
-
-def sort_and_stringify_col(col):
-    """Sort list col then join together as comma-sep string."""
-    return col.apply(lambda x: ", ".join(map(str, sorted(x))))
+    return df[col_order]
 
 
 def main():
@@ -108,56 +137,41 @@ def main():
     syn = utils.syn_login()
     args = get_args()
 
-    manifest = (
-        syn.tableQuery(f"SELECT * FROM {args.manifest}")
-        .asDataFrame()
-        .fillna("")
-    )
-    manifest["grantNumber"] = sort_and_stringify_col(
-        manifest["datasetGrantNumber"])
-    curr_datasets = (
-        syn.tableQuery(
-            f"SELECT datasetAlias, grantNumber FROM {args.portal_table}")
-        .asDataFrame()
-    )
-    curr_datasets["grantNumber"] = sort_and_stringify_col(
-        curr_datasets["grantNumber"])
+    if args.dryrun:
+        print("\n❗❗❗ WARNING:", "dryrun is enabled (no updates will be done)\n")
 
-    # Only add datasets not currently in the Publications table, using
-    # dataset alias + grant number to determine uniqueness.
-    new_datasets = (
-        pd.merge(
-            manifest,
-            curr_datasets,
-            how="left",
-            left_on=["datasetAlias", "grantNumber"],
-            right_on=["datasetAlias", "grantNumber"],
-            indicator=True)
-        .query("_merge=='left_only'")
+    manifest = pd.read_csv(syn.get(args.manifest_id).path).fillna("")
+    manifest.columns = manifest.columns.str.replace(" ", "")
+    manifest["grantNumber"] = utils.sort_and_stringify_col(
+        manifest["DatasetGrantNumber"]
     )
-    if new_datasets.empty:
-        print("No new datasets found!")
-    else:
-        print(f"{len(new_datasets)} new datasets found!\n")
-        if args.dryrun:
-            print(u"\u26A0", "WARNING:",
-                  "dryrun is enabled (no updates will be done)\n")
-            print(new_datasets)
-        else:
-            print("Adding new datasets...")
-            grants = (
-                syn.tableQuery(
-                    "SELECT grantId, grantNumber, grantName, theme, consortium FROM syn21918972")
-                .asDataFrame()
-            )
-            pubs = (
-                syn.tableQuery(
-                    "SELECT pubMedId, publicationTitle FROM syn21868591")
-                .asDataFrame()
-            )
-            new_datasets = add_missing_info(syn, new_datasets, grants, pubs)
-            sync_table(syn, new_datasets, args.portal_table)
-    print("DONE ✓")
+    if args.verbose:
+        print("🔍 Preview of manifest CSV:\n" + "=" * 72)
+        print(manifest)
+        print()
+
+    print("Processing dataset staging database...")
+    grants = syn.tableQuery(
+        "SELECT grantId, grantNumber, grantName, theme, consortium FROM syn21918972"
+    ).asDataFrame()
+    pubs = syn.tableQuery(
+        "SELECT pubMedId, publicationTitle FROM syn21868591"
+    ).asDataFrame()
+
+    database = add_missing_info(manifest, grants, pubs)
+    final_database = clean_table(database)
+    if args.verbose:
+        print("\n🔍 Dataset(s) to be synced:\n" + "=" * 72)
+        print(final_database)
+        print()
+
+    if not args.dryrun:
+        utils.update_table(syn, args.portal_table_id, final_database)
+        print()
+
+    print(f"📄 Saving copy of final table to: {args.output_csv}...")
+    final_database.to_csv(args.output_csv, index=False)
+    print("\n\nDONE ✅")
 
 
 if __name__ == "__main__":
