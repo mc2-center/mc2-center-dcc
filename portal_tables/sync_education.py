60c81261cf7a73c6fa49b454493369cff7d09761
Orion Banks <49208907+Bankso@users.noreply.github.com>
2024-02-22 14:09:46 -0800

    Add sync_education script

---
+++ portal_tables/sync_education.py
@@ -0,0 +1,93 @@
+"""Add Educational Resources to the Cancer Complexity Knowledge Portal (CCKP).
+
+This script will "sync" the Educational Resource manifest table to the Educational Resource portal
+table, by first truncating the table, then re-adding the rows.
+"""
+
+import pandas as pd
+import utils
+
+def clean_table(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean up the table one final time."""
+
+    # Convert string columns to string-list.
+    for col in [
+        "ResourceTopic",
+        "ResourceActivityType",
+        "ResourcePrimaryFormat",
+        "ResourceIntendedUse",
+        "ResourcePrimaryAudience",
+        "ResourceEducationalLevel",
+        "ResourceOriginInstitution",
+        "ResourceLanguage",
+        "ResourceContributors",
+        "ResourceGrantNumber",
+        "ResourceSecondaryTopic",
+        "ResourceMediaAccessibility",
+        "ResourceAccessHazard"
+    ]:
+        df[col] = utils.convert_to_stringlist(df[col])
+
+    # Ensure columns match the table order.
+    col_order = [
+        "Component",
+        "ResourceTitle",
+        "ResourceLink",
+        "ResourceTopic",
+        "ResourceActivityType",
+        "ResourcePrimaryFormat",
+        "ResourceIntendedUse",
+        "ResourcePrimaryAudience",
+        "ResourceEducationalLevel",
+        "ResourceDescription",
+        "ResourceOriginInstitution",
+        "ResourceLanguage",
+        "ResourceContributors",
+        "ResourceGrantNumber",
+        "ResourceSecondaryTopic",
+        "ResourceLicense",
+        "ResourceUseRequirements",
+        "ResourceAlias",
+        "ResourceInternalIdentifier",
+        "ResourceMediaAccessibility",
+        "ResourceAccessHazard",
+        "ResourceDatasetAlias",
+        "ResourceToolLink"
+    ]
+    return df[col_order]
+
+
+def main():
+    """Main function."""
+    syn = utils.syn_login()
+    args = utils.get_args("education")
+
+    if args.dryrun:
+        print("\n❗❗❗ WARNING:", "dryrun is enabled (no updates will be done)\n")
+
+    manifest = pd.read_csv(syn.get(args.manifest_id).path).fillna("")
+    manifest.columns = manifest.columns.str.replace(" ", "")
+    if args.verbose:
+        print("🔍 Preview of manifest CSV:\n" + "=" * 72)
+        print(manifest)
+        print()
+
+    print("Processing educational resource staging database...")
+    
+    final_database = clean_table(manifest)
+    if args.verbose:
+        print("\n🔍 Tool(s) to be synced:\n" + "=" * 72)
+        print(final_database)
+        print()
+
+    if not args.dryrun:
+        utils.update_table(syn, args.portal_table_id, final_database)
+        print()
+
+    print(f"📄 Saving copy of final table to: {args.output_csv}...")
+    final_database.to_csv(args.output_csv, index=False)
+    print("\n\nDONE ✅")
+
+
+if __name__ == "__main__":
+    main()
