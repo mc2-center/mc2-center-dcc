ff36f4f62f3fe035096ef370fa92daefc60e95aa
verena <9377970+vpchung@users.noreply.github.com>
2024-02-23 18:36:48 -0800

    Revert "use bash script"
    This reverts commit 8ce1fce421859ab7e26265de3114bf2634ea6733.

--- run_sync.sh
+++
@@ -1,5 +0,0 @@
-python portal_tables/sync_publications.py --noprint --dryrun
-echo "----------"
-python portal_tables/sync_datasets.py --noprint --dryrun
-echo "----------"
-python portal_tables/sync_tools.py --noprint --dryrun
