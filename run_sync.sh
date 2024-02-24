8ce1fce421859ab7e26265de3114bf2634ea6733
verena <9377970+vpchung@users.noreply.github.com>
2024-02-23 18:33:55 -0800

    use bash script

---
+++ run_sync.sh
@@ -0,0 +1,5 @@
+python portal_tables/sync_publications.py --noprint --dryrun
+echo "----------"
+python portal_tables/sync_datasets.py --noprint --dryrun
+echo "----------"
+python portal_tables/sync_tools.py --noprint --dryrun
