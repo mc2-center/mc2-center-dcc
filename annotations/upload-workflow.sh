76fa656ec7bd53ca31fb00b6d7711e157b4fcdd0
aditigopalan <63365451+aditigopalan@users.noreply.github.com>
2024-07-10 15:11:22 -0400

    Adding ID generation into script

--- annotations/upload-workflow.sh
+++ annotations/upload-workflow.sh
@@ -6,14 +6,28 @@
 echo "Activating conda environment: schematic"
 conda activate schematic
 
+<<<<<<< Updated upstream
 # Check if activation was successful
 if [ $? -eq 0 ]; then
     # Run python3 split_manifest_grants.py
     echo "Splitting manifests..."
     python3 split_manifest_grants.py manifest_sep.csv publication ./output/output_sep --csv
+=======
+
+# Run python3 split_manifest_grants.py
+echo "Splitting manifests..."
+python3 split_manifest_grants.py april-datasets.csv dataset ./output/output_april --csv
+
+# Check if split_manifest_grants.py was successful
+if [ $? -eq 0 ]; then
+    # Run python3 script_name.py
+    echo "Generating file paths..."
+    python3 gen-mp-csv.py ./output/output_april april_datasets_filepaths.csv datasets
+>>>>>>> Stashed changes
 
     # Check if split_manifest_grants.py was successful
     if [ $? -eq 0 ]; then
+<<<<<<< Updated upstream
         # Run python3 script_name.py
         echo "Generating file paths..."
         python3 gen-mp-csv.py ./output/output_june june_filepaths.csv publication
@@ -29,6 +43,24 @@ if [ $? -eq 0 ]; then
                 # Run python3 schema_update.py
                 echo "Running schema updates..."
                 python3 schema_update.py june_filepaths.csv Publication
+=======
+        # Run python3 combined_script.py
+        echo "Processing split files..."
+        python3 processing-splits.py ./output/output_april
+        # Check if combined_script.py was successful
+        if [ $? -eq 0 ]; then
+            # Run python3 schema_update.py
+            # echo "Running schema updates..."
+            # python3 schema_update.py april_datasets_filepaths.csv Dataset
+
+            echo "Generating Dataset/ Tool/ Educational Resource folder Ids"
+            python3 create_id_folders.py -m april_datasets_filepaths.csv -t DatasetView
+
+            if [ $? -eq 0 ]; then
+                # Run python3 upload-manifests.py
+                echo "Uploading manifests..."
+                python3 upload-manifests.py -m april_datasets_filepaths.csv -t DatasetView -c ./config.yml
+>>>>>>> Stashed changes
 
                 # Check if schema_update.py was successful
                 if [ $? -eq 0 ]; then
