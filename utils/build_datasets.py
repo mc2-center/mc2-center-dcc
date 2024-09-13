7a4b272212cd5c3e19dd26b2ff618167184e6301
Orion Banks <49208907+Bankso@users.noreply.github.com>
2024-09-13 16:38:42 -0700

    Update build_datasets.py

--- utils/build_datasets.py
+++ utils/build_datasets.py
@@ -3,13 +3,14 @@
 This script will update the scope of a Synapse Dataset
 
 Usage:
-python build_datasets.py -d <Dataset Synapse Id> -s <Folder Synapse Id containing files for Dataset>
+python build_datasets.py -d <Dataset Synapse Id> -s <Folder Synapse Id containing files for Dataset> -c <CSV with dataset and folder Synapse Ids>
 
 author: orion.banks
 """
 
 import synapseclient
 import argparse
+import pandas as pd
 
 
 def get_args():
@@ -19,11 +20,19 @@ def get_args():
         "-d",
         type=str,
         help="Synapse Id of a Dataset to update with files from input folder",
+        required=False
     )
     parser.add_argument(
         "-s",
         type=str,
         help="Synapse Id of a folder that should have all files added to the Dataset",
+        required=False
+    )
+    parser.add_argument(
+        "-c",
+        type=str,
+        help="Path to a CSV file with Dataset and Folder Synapse Ids.",
+        required=False
     )
     return parser.parse_args()
 
@@ -36,13 +45,35 @@ def main():
 
     args = get_args()
 
-    datasetId, scopeId = args.d, args.s  # assign path to manifest file from command line input
+    datasetId, scopeId, idSheet= args.d, args.s, args.c  # assign path to manifest file from command line input
 
-    dataset = syn.get(datasetId)
+    if idSheet:
+        idSet = pd.read_csv(idSheet, header=None)
+        if idSet.iat[0,0] == "DatasetView_id" and idSet.iat[0,1] == "Folder_id":
+            print(f"\nInput sheet read successfully...\n\nCreating Datasets now:")
+            idSet = idSet.iloc[1:,:]
+            count = 0
+            for row in idSet.itertuples(index=False):
+                datasetId = row[0]
+                scopeId = row[1]
+                dataset = syn.get(datasetId)
+                dataset.add_folder(scopeId, force=True)
+                dataset = syn.store(dataset)
+                print(f"\nDataset {datasetId} successfully updated with files from {scopeId}")
+                count += 1
+            print(f"\n\nDONE ✅\n{count} Datasets processed")
+        else:
+            print(f"\n❗❗❗ The table provided does not appear to be formatted for this operation.❗❗❗\nPlease check its contents and try again.")
+    
+    else:
+        if datasetId and scopeId:
+            dataset = syn.get(datasetId)
 
-    dataset.add_folder(scopeId, force=True)
+            dataset.add_folder(scopeId, force=True)
 
-    dataset = syn.store(dataset)
+            dataset = syn.store(dataset)
+        else:
+            print(f"\n❗❗❗ No dataset information provided.❗❗❗\nPlease check your command line inputs and try again.")
 
 
 if __name__ == "__main__":
