91179764385db91db113fe90db08a83b181ec2f7
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-01-15 16:01:35 -0800

    Update upload-manifests.py
    Add option to skip validation and accompanying logic

--- annotations/upload-manifests.py
+++ annotations/upload-manifests.py
@@ -38,6 +38,12 @@ def get_args():
         action="store_false",
         help="Boolean; if this flag is provided, invalid manifests will be submitted",
     )
+    parser.add_argument(
+        "--skip",
+        action="store_true",
+        default=None,
+        help="Boolean; if this flag is provided, validation will be skipped. Only use if your manifests have been previously validated.",
+    )
     return parser.parse_args()
 
 
@@ -115,38 +121,45 @@ def main():
     config_file = args.c
     manifest_type = args.t
     submit_valid = args.v
+    skip_validation = args.skip
 
     df = pd.read_csv(csv_file)
     pd.set_option("display.max_colwidth", 90)
 
     num_processes = multiprocessing.cpu_count()  # Number of CPU cores
-    pool = multiprocessing.Pool(processes=num_processes)
-
+    
     validation_args_list = list(df.to_records(index=False))
 
-    validated_files = pool.map(
-        partial(
-            validate_entry_worker,
-            cf=config_file,
-            mt=manifest_type,
-            valid_only=submit_valid,
-        ),
-        validation_args_list,
-    )
-    print("/n ####VALIDATED FILES##### /n", validated_files)
+    if skip_validation is not None:
+        submit_args_list = validation_args_list
 
-    pool.close()
-    pool.join()
+    if skip_validation is None:
 
-    validated_files = [tup for tup in validated_files if tup is not None]
+        pool = multiprocessing.Pool(processes=num_processes)
+
+        validated_files = pool.map(
+            partial(
+                validate_entry_worker,
+                cf=config_file,
+                mt=manifest_type,
+                valid_only=submit_valid,
+            ),
+            validation_args_list,
+        )
+        print("/n ####VALIDATED FILES##### /n", validated_files)
+
+        pool.close()
+        pool.join()
+
+        validated_files = [tup for tup in validated_files if tup is not None]
+        submit_args_list = [tup for tup in validated_files]
 
     submit_pool = multiprocessing.Pool(processes=num_processes)
-
-    submit_args_list = [tup for tup in validated_files]
+    
     print(pd.DataFrame(submit_args_list))
 
     choice = input(
-        "\n\nReview the printed list of arguments for errors in path and target matches. Type 'upload' to continue or 'end' if you see an error"
+        "\n\nReview the printed list of arguments for errors in path and target matches. Type 'upload' to continue or 'end' if you see an error\n\n"
     )
 
     if choice == "upload":
