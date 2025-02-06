60d09627535a580359240e9497fc5659b44881b8
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-02-05 17:46:28 -0800

    Update processing-splits.py
    Add column ordering

--- annotations/processing-splits.py
+++ annotations/processing-splits.py
@@ -15,7 +15,7 @@ import sys
 
 def process_csv(file_path):
     # Read the CSV file into a DataFrame
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, header=0)
 
     # 1. Change column name from "Publication TumorType" to "Publication Tumor Type"
     if "Publication TumorType" in df.columns:
@@ -39,6 +39,133 @@ def process_csv(file_path):
                     else x
                 )
             )
+    # 5. Reorder columns to match the table order
+    # Get data type based on Component
+    data = df.iloc[0,0]
+    if data == "PublicationView":
+        col_order = [
+            "Component",
+            "PublicationView_id",
+            "GrantView Key",
+            "Study Key",
+            "Publication Doi",
+            "Publication Journal",
+            "Pubmed Id",
+            "Pubmed Url",
+            "Publication Title",
+            "Publication Year",
+            "Publication Keywords",
+            "Publication Authors",
+            "Publication Abstract",
+            "Publication Assay",
+            "Publication Tumor Type",
+            "Publication Tissue",
+            "Publication Accessibility",
+            "Publication Dataset Alias"
+            ]
+
+    elif data == "DatasetView":
+        col_order = [
+            "Component",
+            "DatasetView_id",
+            "GrantView Key",
+            "Study Key",
+            "PublicationView Key",
+            "Dataset Name",
+            "Dataset Alias",
+            "Dataset Description",
+            "Dataset Design",
+            "Dataset Assay",
+            "Dataset Species",
+            "Dataset Tumor Type",
+            "Dataset Tissue",
+            "Dataset Url",
+            "Dataset Doi",
+            "Dataset File Formats",
+            "Data Use Codes"
+            ]
+
+    elif data == "ToolView":
+        col_order = [
+            "Component",
+            "ToolView_id",
+            "GrantView Key",
+            "Study Key",
+            "DatasetView Key",
+            "PublicationView Key",
+            "Tool Name",
+            "Tool Description",
+            "Tool Homepage",
+            "Tool Version",
+            "Tool Operation",
+            "Tool Input Data",
+            "Tool Output Data",
+            "Tool Input Format",
+            "Tool Output Format",
+            "Tool Function Note",
+            "Tool Cmd",
+            "Tool Type",
+            "Tool Topic",
+            "Tool Operating System",
+            "Tool Language",
+            "Tool License",
+            "Tool Cost",
+            "Tool Accessibility",
+            "Tool Download Url",
+            "Tool Download Type",
+            "Tool Download Note",
+            "Tool Download Version",
+            "Tool Documentation Url",
+            "Tool Documentation Type",
+            "Tool Documentation Note",
+            "Tool Link Url",
+            "Tool Link Type",
+            "Tool Link Note",
+            "Tool Doi",
+            "Tool Date Last Modified",
+            "Tool Release Date",
+            "Tool Package Dependencies",
+            "Tool Package Dependencies Present",
+            "Tool Compute Requirements",
+            "Tool Entity Name",
+            "Tool Entity Type",
+            "Tool Entity Role"
+            ]
+
+    elif data == "EducationalResource":
+        col_order = [
+            "Component",
+            "EducationalResource_id",
+            "GrantView Key",
+            "Study Key",
+            "DatasetView Key",
+            "PublicationView Key",
+            "ToolView Key",
+            "Resource Title",
+            "Resource Link",
+            "Resource Doi",
+            "Resource Topic",
+            "Resource Activity Type",
+            "Resource Primary Format",
+            "Resource Intended Use",
+            "Resource Primary Audience",
+            "Resource Educational Level",
+            "Resource Description",
+            "Resource Origin Institution",
+            "Resource Language",
+            "Resource Contributors",
+            "Resource Secondary Topic",
+            "Resource License",
+            "Resource Use Requirements",
+            "Resource Alias",
+            "Resource Internal Identifier",
+            "Resource Media Accessibility",
+            "Resource Access Hazard",
+            "Resource Dataset Alias",
+            "Resource Tool Link"
+        ]
+
+    df = df[col_order]
 
     # Save the modified DataFrame back to the CSV file
     df.to_csv(file_path, index=False)
