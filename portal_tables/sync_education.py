3645bf2089bbfa2111dee9e6f5cbf4eb3266f440
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-02-18 15:16:49 -0800

    Update sync_education.py
    Added columns for CCKP table schema
    Added code to remove unused columns
    Added logic to parse link and alias for Synapse-related patterns. Script will preferentially check if alias is a synId. If true, a new Synapse URL will be constructed and listed in synapseLink. If the alias is not a synId, the script will check if the Resource Link is a Synapse URL and copy it to the synapseLink column, if true. Resource links will have the format [synId](URL) for alias match and [Link](URL) if a URL match

--- portal_tables/sync_education.py
+++ portal_tables/sync_education.py
@@ -5,12 +5,48 @@ table, by first truncating the table, then re-adding the rows.
 """
 
 import pandas as pd
+import re
 import utils
 
+def add_missing_info(
+    education: pd.DataFrame) -> pd.DataFrame:
+    """Add missing information into table before syncing."""
+
+    url_pattern = re.compile(".*(synapse\.org).*")
+    alias_pattern = re.compile("^syn\d{7,8}$")
+    education["synapseLink"] = ""
+    for _, row in education.iterrows():
+        is_in_synapse = None
+        alias_list = row["ResourceAlias"].split(",")
+        link_list = row["ResourceLink"].split(",")
+        syn_link_list = []
+        for a in alias_list:
+            a_match = re.match(alias_pattern, a)
+            is_in_synapse = True if a_match else None
+            if is_in_synapse:
+                syn_link = "".join(["https://www.synapse.org/Synapse:", a])
+                formatted_syn_link = "".join(["[", a, "](", syn_link, ")"])
+                syn_link_list.append(formatted_syn_link)
+                syn_links = ",".join(syn_link_list)
+                education.at[_, "synapseLink"] = syn_links
+            else:
+                for s in link_list:
+                    s_match = re.match(url_pattern, s)
+                    is_in_synapse = True if s_match else None
+                    if is_in_synapse:
+                        education.at[_, "synapseLink"] = "".join(["[Link](", education.at[_, "ResourceLink"], ")"])
+   
+    return education
 
 def clean_table(df: pd.DataFrame) -> pd.DataFrame:
     """Clean up the table one final time."""
-
+    
+    df["ResourceGrantNumber"] = df["GrantViewKey"]
+    df["ResourcePubmedId"] = df["PublicationViewKey"]
+    df["ResourceDatasetAlias"] = df["DatasetViewKey"]
+    df["ResourceToolLink"] = df["ToolViewKey"]
+    df = df.drop(["GrantViewKey", "PublicationViewKey", "DatasetViewKey", "ToolViewKey", "StudyKey"], errors="ignore")
+    
     # Convert string columns to string-list.
     for col in [
         "ResourceTopic",
@@ -26,6 +62,8 @@ def clean_table(df: pd.DataFrame) -> pd.DataFrame:
         "ResourceSecondaryTopic",
         "ResourceMediaAccessibility",
         "ResourceAccessHazard",
+        "ResourcePubmedId",
+        "ResourceDatasetAlias"
     ]:
         df[col] = utils.convert_to_stringlist(df[col])
 
@@ -54,6 +92,9 @@ def clean_table(df: pd.DataFrame) -> pd.DataFrame:
         "ResourceAccessHazard",
         "ResourceDatasetAlias",
         "ResourceToolLink",
+        "ResourceDoi",
+        "synapseLink",
+        "ResourcePubmedId"
     ]
     return df[col_order]
 
@@ -74,8 +115,8 @@ def main():
         print()
 
     print("Processing educational resource staging database...")
-
-    final_database = clean_table(manifest)
+    database = add_missing_info(manifest)
+    final_database = clean_table(database)
     if args.verbose:
         print("\n🔍 Educational resource(s) to be synced:\n" + "=" * 72)
         print(final_database)
