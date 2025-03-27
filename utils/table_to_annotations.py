820e8be78bd1ccccaaf17c6f72c1cdd8125836fc
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-03-27 14:11:50 -0700

    Update table_to_annotations.py

--- utils/table_to_annotations.py
+++ utils/table_to_annotations.py
@@ -31,37 +31,35 @@ def get_args():
     parser.add_argument(
         "-s",
         type=str,
-        help="Path to a CSV file with entity Synapse Ids and Components on each row",
+        help="Synapse Id of a table containing the data that should be converted to annotations.",
+        required=False
+    )
+    parser.add_argument(
+        "-m",
+        type=str,
+        help="Path to a CSV with the source table Synapse Ids and entity Ids to be annotated",
         required=False
     )
     return parser.parse_args()
 
 
-def map_table_to_schema(syn, component, table_id, dataset_id):
-
+def build_annotations(syn, component, source_id, target_id, mapping):
     
-    #mapping = mapping_dict #define here or pull from data-models
-    entity = syn.get(dataset_id)
-    query = f"SELECT * FROM {table_id} WHERE {component}_id = '{dataset_id}'"
+    entity = syn.get(target_id, downloadFile=False)
+    query = f"SELECT * FROM {source_id} WHERE {component}_id = '{target_id}'"
     annotations_to_add = syn.tableQuery(query).asDataFrame()
-    print("Annotation table acquired from Synapse")
-    #get Dataset View annotations from table. Suggest using union table; ensure scope is updated before pulling
-    #make DatasetView_id the index
-    #replace column names with schema key names and reference in assignment below
+    print(f"Annotations acquired from Synapse table {source_id} for entity: {target_id}")
+    if mapping is not None:
+        annotations_to_add = map_annotations()
     annotations_to_add = annotations_to_add.to_dict()
-    print(annotations_to_add)
-    new_annotations = Annotations(dataset_id, entity.etag, annotations_to_add) #pull current annotations - should be empty if schema was just bound
-    #print("Current target annotations acquired from Synapse")
-    print(new_annotations)
-    
-    #synapse_annotations = syn.to_synapse_annotations(annotations_to_add)
-    #for key in current_annotations: #determine correct reference, but want to loop through annotation keys and add values from table
-     #   print("Updating target annotations")
-      #  current_annotations[key] = annotations_to_add.at([dataset_id, key])
-       # print(current_annotations)
-    
+    new_annotations = Annotations(target_id, entity.etag, annotations_to_add)
+
     new_annotations = syn.set_annotations(new_annotations)
-   
+    print(f"Annotations applied to Synapse entity: {target_id}")
+
+def map_annotations():
+    None
+
 def main():
 
     syn = (
@@ -70,26 +68,26 @@ def main():
 
     args = get_args()
 
-    target, component, version, sheet= args.t, args.c, args.v, args.s
+    target, component, source, sheet = args.t, args.c, args.v, args.s, args.m
     
     if sheet:
         idSet = pd.read_csv(sheet, header=None)
         if idSet.iat[0,0] == "entity" and idSet.iat[0,1] == "component":
-            print(f"\nInput sheet read successfully!\n\nBinding schemas now...")
+            print(f"\nInput sheet read successfully!\n\nApplying annotations now...")
             idSet = idSet.iloc[1:,:]
             count = 0
             for row in idSet.itertuples(index=False):
                 target = row[0]
                 component = row[1]
-                annotations = map_table_to_schema(syn, component, "syn52752399", target)  
+                annotations = build_annotations(syn, component, source, target)  
                 count += 1
-            print(f"\n\nDONE ✅\n{count} schemas bound")
+            print(f"\n\nDONE ✅\n{count} entities had annotations applied.")
         else:
             print(f"\n❗❗❗ The table provided does not appear to be formatted for this operation.❗❗❗\nPlease check its contents and try again.")
     
     else: #if no sheet provided, run process for one round of inputs only
         if target and component:
-            annotations = map_table_to_schema(syn, component, "syn52752399", target)
+            annotations = build_annotations(syn, component, source, target)
         else:
             print(f"\n❗❗❗ No dataset information provided.❗❗❗\nPlease check your command line inputs and try again.")
 
