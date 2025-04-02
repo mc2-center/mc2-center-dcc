bdfecf05b855a4fcd2a083382772a40af4f3115e
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-04-02 14:09:54 -0700

    Update synapse_json_schema_bind.py

--- utils/synapse_json_schema_bind.py
+++ utils/synapse_json_schema_bind.py
@@ -2,7 +2,7 @@
 
 This script will create and bind a JSON schema to an entity
 
-Usage:
+Usage: python synapse_json_schema_bind.py -t [Entity Synapse Id] -c [Schema data type] -v [Data model release version] -g [Grant number in CA format] -s [Path to CSV for schema binding in bulk]
 
 author: orion.banks
 """
@@ -50,7 +50,9 @@ def get_args():
     return parser.parse_args()
 
 
-def get_schema_organization(service):
+def get_schema_organization(service) -> tuple:
+    """Create or access the MC2 Center Synapse organization,
+    return a tuple of schema service object, organization object, and organization name"""
 
     org_name = "Multi Consortia Coordinating Center"
     
@@ -66,11 +68,16 @@ def get_schema_organization(service):
     return service, schema_org, org_name
 
 
-def register_json_schema(org, schema_type, schema_json, version, schema_org_name):
+def register_json_schema(org, schema_type: str, schema_json: json, version: str, schema_org_name: str) -> str:
+    """Register or access a previously registered JSON schema and return the uri.
+    If the schema was previously registered, the constructed uri will be returned.
+    uri format: [schema_org_name]-[schema_type]-[num_version]
+    Example uri: MultiConsortiaCoordinatingCenter-CA987654duoCodeAR-2.0.0
+    """
     
     num_version = version.split("v")[1]
 
-    uri = "-".join([schema_org_name,schema_type,num_version])
+    uri = "-".join([schema_org_name.replace(" ", ""),schema_type,num_version])
 
     try:
         schema = org.create_json_schema(schema_json, schema_type, semantic_version=num_version)
@@ -83,7 +90,10 @@ def register_json_schema(org, schema_type, schema_json, version, schema_org_name
     return uri
 
 
-def bind_schema_to_entity(syn, service, schema_uri, entity_id, component_type):
+def bind_schema_to_entity(syn, service, schema_uri: str, entity_id: str, component_type: str):
+    """Associate a registered JSON schema with a Synapse entity.
+    For JSON schemas associated with DUO-based access restrictions, use the REST API and enable derived annotations,
+    For non-AR schemas, use the python client bind_json_schema function"""
 
     if component_type != "duoCodeAR":
         print(f"Binding non-AR schema {schema_uri}")
@@ -101,7 +111,15 @@ def bind_schema_to_entity(syn, service, schema_uri, entity_id, component_type):
         )
 
    
-def get_schema_from_url(component, version, grant):
+def get_schema_from_url(component: str, version: str, grant: str) -> tuple[any, str]:
+    """Access a JSON schema stored in the MC2 Center data-models GitHub repo,
+    based on the version release tag, data type, and grant number (if of type duoCodeAR).
+    Return request JSON and adjusted component name.
+
+    Note that the filename must match expected conventions:
+    Non-AR schema example: mc2.DatasetView.schema.json
+    AR schema example: mc2.CA987654duoCodeAR.schema.json
+    """
 
     #base_schema_url = "".join(["https://raw.githubusercontent.com/mc2-center/data-models/refs/tags/v", version, "/json_schemas/"])
     base_schema_url = "".join(["https://raw.githubusercontent.com/mc2-center/data-models/refs/heads/136-173-dataset-schema/json_schemas/"])
@@ -122,7 +140,8 @@ def get_schema_from_url(component, version, grant):
     return schema_json, component
 
 
-def get_register_bind_schema(syn, component, grant, version, target, schema_org_name, org, service):
+def get_register_bind_schema(syn, component: str, grant: str, version: str, target: str, schema_org_name: str, org, service):
+    """Access JSON from URL, register the JSON schema, and bind the schema to the target entity."""
     
     schema_json, component_adjusted = get_schema_from_url(component, version, grant)
     print(f"Registering JSON schema {component_adjusted} {version}")
