986abaf3bb25a804d7c9ff5ff3a9b89ef3cee28e
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-04-03 12:20:39 -0700

    Update build_datasets.py

--- utils/build_datasets.py
+++ utils/build_datasets.py
@@ -44,15 +44,17 @@ def get_args():
     )
     return parser.parse_args()
 
-def filter_files_in_folder(syn, scopeId, formats, dataset):
+
+def filter_files_in_folder(syn, scopeId: str, formats: list, dataset):
+    """Capture all files in provided scope and select files that match a list of formats,
+    add files with matching format(s) to dataset object,
+    return dataset object"""
 
     all_files = []
     walkPath = synapseutils.walk(syn, scopeId, ["file"])
     for dirpath, dirname, filename in walkPath:
         all_files = all_files + filename
-    print(all_files)
     files = [file[1] for file in all_files for format in formats if format in file[0]]
-    print(files)
     dataset_items = [{"entityId":f, "versionNumber":syn.get(f, downloadFile=False).versionLabel} for f in files]
     dataset.add_items(dataset_items)
 
@@ -60,42 +62,40 @@ def filter_files_in_folder(syn, scopeId, formats, dataset):
 
 def main():
 
-    syn = (
-        synapseclient.login()
-    )  # you can pass your username and password directly to this function
+    syn = synapseclient.login()
 
     args = get_args()
 
-    datasetId, scopeId, idSheet, formats = args.d, args.s, args.c, args.f  # assign path to manifest file from command line input
+    dataset_id, scope_id, id_sheet, formats = args.d, args.s, args.c, args.f
 
-    if idSheet:
-        idSet = pd.read_csv(idSheet, header=None)
-        if idSet.iat[0,0] == "DatasetView_id" and idSet.iat[0,1] == "Folder_id":
+    if id_sheet:
+        id_set = pd.read_csv(id_sheet, header=None)
+        if id_set.iat[0,0] == "DatasetView_id" and id_set.iat[0,1] == "Folder_id":
             print(f"\nInput sheet read successfully...\n\nCreating Datasets now:")
-            idSet = idSet.iloc[1:,:]
             count = 0
-            for row in idSet.itertuples(index=False):
-                datasetId = row[0]
-                scopeId = row[1]
-                dataset = syn.get(datasetId)
-                if formats is not None:
-                    dataset = filter_files_in_folder(syn, scopeId, formats, dataset)
+            for _, row in id_set.iterrows():
+                dataset_id = row["DatasetView_id"]
+                scope_id = row["Folder_id"]
+                formats = list(row["format_list"])
+                dataset = syn.get(dataset_id)
+                if len(formats) > 0:
+                    dataset = filter_files_in_folder(syn, scope_id, formats, dataset)
                 else:
-                    dataset.add_folder(scopeId, force=True)
+                    dataset.add_folder(scope_id, force=True)
                 dataset = syn.store(dataset)
-                print(f"\nDataset {datasetId} successfully updated with files from {scopeId}")
+                print(f"\nDataset {dataset_id} successfully updated with files from {scope_id}")
                 count += 1
             print(f"\n\nDONE ✅\n{count} Datasets processed")
         else:
             print(f"\n❗❗❗ The table provided does not appear to be formatted for this operation.❗❗❗\nPlease check its contents and try again.")
     
     else:
-        if datasetId and scopeId:
-            dataset = syn.get(datasetId)
+        if dataset_id and scope_id:
+            dataset = syn.get(dataset_id)
             if formats is not None:
-                dataset = filter_files_in_folder(syn, scopeId, formats, dataset)
+                dataset = filter_files_in_folder(syn, scope_id, formats, dataset)
             else:    
-                dataset.add_folder(scopeId, force=True)
+                dataset.add_folder(scope_id, force=True)
             dataset = syn.store(dataset)
         else:
             print(f"\n❗❗❗ No dataset information provided.❗❗❗\nPlease check your command line inputs and try again.")
