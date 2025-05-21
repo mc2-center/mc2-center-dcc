705edb2e79e6f484e137df391fdd658ecfdcd9e2
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-05-21 13:22:32 -0700

    Add multi_condition parameter
    Passing -m at runtime will generate a JSON schema with additional conditions beyond dataUseModifiers, as defined in the AR data dictionary CSV. If -m is not given, then JSON schema "if-then" statement will only be controlled by the value of annotation "dataUseModifiers"

--- utils/generate_duo_schema.py
+++ utils/generate_duo_schema.py
@@ -3,7 +3,7 @@ import json
 import argparse
 from collections import OrderedDict
 
-def build_condition(row, col_names):
+def build_condition(row, col_names, multi_condition):
     """
     Builds a JSON Schema if-then rule based on a row from the data dictionary.
     """
@@ -37,14 +37,15 @@ def build_condition(row, col_names):
     additional_conditions = {}
     required_fields = ["dataUseModifiers"]
 
-    if "activatedByAttribute" in row and pd.notna(row["activatedByAttribute"]):
-        additional_conditions[row["activatedByAttribute"]] = { "type": "array", "items": { "type": "string" }, "contains": { "const": row["activationValue"] } }
-        required_fields.append(row["activatedByAttribute"])
+    if multi_condition is not None:
+        if "activatedByAttribute" in row and pd.notna(row["activatedByAttribute"]):
+            additional_conditions[row["activatedByAttribute"]] = { "type": "array", "items": { "type": "string" }, "contains": { "const": row["activationValue"] } }
+            required_fields.append(row["activatedByAttribute"])
 
-    for col in col_names:
-        if col in row and pd.notna(row[col]):
-            additional_conditions[col] = { "type": "array", "items": { "type": "string" }, "contains": { "const": row[col] } }
-            required_fields.append(col)
+        for col in col_names:
+            if col in row and pd.notna(row[col]):
+                additional_conditions[col] = { "type": "array", "items": { "type": "string" }, "contains": { "const": row[col] } }
+                required_fields.append(col)
 
     if additional_conditions:
         condition["if"]["properties"].update(additional_conditions)
@@ -53,7 +54,10 @@ def build_condition(row, col_names):
     return condition
 
 
-def generate_json_schema(csv_path, output_path, title, version, org_id, grant_id):
+def generate_json_schema(csv_path, output_path, title, version, org_id, grant_id, multi_condition):
+    """
+    Generates a JSON Schema from a CSV file containing DUO-based access restrictions.
+    """
     df = pd.read_csv(csv_path, header=0, dtype=str)
 
     conditions = []
@@ -63,7 +67,7 @@ def generate_json_schema(csv_path, output_path, title, version, org_id, grant_id
     for _, row in df.iterrows():
         if grant_id != "Project" and row["grantNumber"] != grant_id:
             continue
-        condition = build_condition(row, col_names)
+        condition = build_condition(row, col_names, multi_condition)
         conditions.append(condition)
 
     schema = OrderedDict({
@@ -88,8 +92,9 @@ if __name__ == "__main__":
     parser.add_argument("-v", "--version", default="v1.0.0", help="Schema version")
     parser.add_argument("-o", "--org_id", default="mc2", help="Organization ID for $id field")
     parser.add_argument("-g", "--grant_id", help="Grant number to select conditions for from reference table")
+    parser.add_argument("-m", "--multi_condition", help="Generate schema with multiple conditions defined in the CSV", action="store_true", default=None)
 
     args = parser.parse_args()
 
     output_path = "".join([args.output_path, "/", args.org_id, ".", "AccessRequirement-", f"{args.grant_id}-" if args.grant_id else "Project-", args.version, "-schema.json"])
-    generate_json_schema(args.csv_path, output_path, args.title, args.version, args.org_id, grant_id = args.grant_id if args.grant_id else "Project")
+    generate_json_schema(args.csv_path, output_path, args.title, args.version, args.org_id, grant_id = args.grant_id if args.grant_id else "Project", multi_condition=args.multi_condition)
