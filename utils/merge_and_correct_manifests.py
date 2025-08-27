8515b2300dfa989e5cc67157e0d4b00226c24d74
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-08-27 11:38:52 -0700

    CCKP database QC + sync script updates (#112)
    * Update merge_and_correct_manifests.py
    
    - add function to remove whitespace from list columns in DatasetView and PublicationView. The set of column names screened for whitespace can be updated in the script directly.
    - adjust input options and logic in main to ensure this script can be run without an updated manifest, as a way to clean the database without modifying content
    
    * Add function to standardize DOI entries for pubs
    
    If DOI is missing, record "No DOI Listed" as the value
    If a DOI entry is present but missing "https://doi.org/", add "https://doi.org/" to the DOI
    Only applies to PublicationView
    
    * Update docstrings
    
    * Update union_qc.py
    
    - adjust sorting in updated CSVs to simplify review
    - update formatting for printed messages
    - adjust input logic to handle secondary validation of updated entries
    
    * Update union_qc.py
    
    - adjust sorting logic to only occur if source labels were added
    
    * Drop unneeded columns from updated manifest
    
    Instead of having to manually remove the entityId, Source, and iconTags columns from <component>_updated.csv, the script will now automatically remove them.
    
    * Modifying manifest filtering (#115)
    
    Entries in updated manifest will be filtered according to the following logic:
    if two entries exist and one is labeled "Database", keep this entry and do not retain the other in the database
    if only one entry exists (either "Database" or "Updated") keep it in the database
    if only one entry exists and it is labeled "Updated", add it to a separate dataframe, which is returned for review as a CSV
    
    Updated criteria for selecting columns to fill with Pending Annotation or Not Applicable
    
    * Update merge_and_correct_manifests.py
    
    - update docstrings
    - update pattern matching to find DOI fragments

--- utils/merge_and_correct_manifests.py
+++ utils/merge_and_correct_manifests.py
@@ -1,15 +1,20 @@
 """merge_and_correct_manifests.py
 
-This script replaces entries in a metadata manifest based on a primary key field.
-It also fills empty cells in the updated database with 'Pending Annotation' or 'Not Applicable'
-for specific columns, depending on the data type.
+This script performs the following operations on a manifest CSV:
+- removes duplicate entries in Updated input manifest
+- reports Updated entries that are included in the merged and corrected manifest
+- replaces entries based on a primary key field, using a corrected manifest
+- fills empty cells in the updated database with 'Pending Annotation' or 'Not Applicable' for specific columns, depending on the data type.
+- removes leading and trailing whitespace for list columns in Publication View and Dataset View metadata
 
 author: orion.banks
 """
 
 import argparse
+from datetime import datetime
 import os
 import pandas as pd
+import re
 
 def get_args():
     """Set up command-line interface and get arguments."""
@@ -25,10 +30,32 @@ def get_args():
         "-n",
         type=str,
         help="Path to CSV with corrected metadata entries.",
-        required=True,
+        required=False,
         default=None,
     )
     return parser.parse_args()
+
+
+def filter_updated_manifest(new_entries_df: pd.DataFrame, index_col: str, data_type: str) -> pd.DataFrame:
+    """Remove duplicate entries in updated manifest. Retain current Database entries and new Updated entries."""
+    filtered_entries_df = pd.DataFrame(columns=new_entries_df.columns)
+    updated_entries_df = pd.DataFrame(columns=new_entries_df.columns)
+    updated_count = 0
+
+    index_groups = new_entries_df.groupby(index_col, as_index=False)
+    for name, rows in index_groups.groups.items():
+        row_to_keep = index_groups.get_group(name)
+        if len(rows) > 1:
+            row_to_keep = row_to_keep[row_to_keep["Source"].isin(["Database"])]
+        else:
+            if row_to_keep["Source"].isin(["Updated"]).all():
+                updated_entries_df = pd.concat([updated_entries_df, row_to_keep])
+                updated_count += 1
+        filtered_entries_df = pd.concat([filtered_entries_df, row_to_keep])
+
+    updated_entries_df.to_csv(f"{os.getcwd()}/{data_type}_new_rows_{datetime.now().strftime('%Y%m%d')}.csv", index=False)
+
+    return filtered_entries_df
 
 
 def update_database(database_df: pd.DataFrame, new_entries_df: pd.DataFrame, index_col: str) -> pd.DataFrame:
@@ -62,9 +89,11 @@ def fill_empty_cells(updated_database: pd.DataFrame, data_type: str) -> pd.DataF
         "Data Use Codes"       
     ]
 
+    current_cols_to_fill = [col for col in updated_database.columns if col in cols_to_fill]
+
     for _,row in updated_database.iterrows():
-        for col in updated_database.columns:
-            if pd.isna(row[col]) or row[col] == "" and col in cols_to_fill:
+        for col in current_cols_to_fill:
+            if pd.isna(row[col]) or row[col] == "" or re.match(r"^,+", row[col]):
                 if data_type == "PublicationView" and row["Publication Accessibility"] == "Open Access":
                     value = "Not Applicable"
                 else:
@@ -74,33 +103,88 @@ def fill_empty_cells(updated_database: pd.DataFrame, data_type: str) -> pd.DataF
     return updated_database
 
 
+def trim_whitespace(database: pd.DataFrame) -> pd.DataFrame:
+    """Remove leading and trailing whitespace from entries in curated columns."""
+    
+    cols_to_clean = [
+        "Publication Assay",
+        "Publication Tumor Type",
+        "Publication Tissue",
+        "Dataset Assay",
+        "Dataset Species",
+        "Dataset Tissue",
+        "Dataset Tumor Type"
+    ]
+    
+    clean_cols = [col for col in database.columns if col in cols_to_clean]
+    
+    for _,row in database.iterrows():
+        for col in clean_cols:
+            database.at[row.name, col] = row[col].strip()
+
+    return database
+
+
+def fix_pub_doi(database: pd.DataFrame) -> pd.DataFrame:
+    """Add 'https://doi.org/' to existing identifiers or record 'No DOI Listed' if cell is empty."""
+
+    for _,row in database.iterrows():
+        if row["Publication Doi"].startswith("https://doi.org/") is False:
+            if row["Publication Doi"] == "":
+                value = "No DOI Listed"
+            elif re.match(r"^\d", row["Publication Doi"]):
+                value = "".join(["https://doi.org/", row["Publication Doi"]])
+            else:
+                value = row["Publication Doi"]
+            database.at[row.name, "Publication Doi"] = value
+            
+    return database
+
+
 def main():
-    """Main function to merge corrected manifests."""
+    """Main function to merge and clean manifests."""
     args = get_args()
 
     database, new_entries = args.d, args.n
 
     for sheet in [database, new_entries]:
-        if os.path.exists(sheet):
-            continue
-        else:
-            print(f"\n❗❗❗ The file {sheet} does not exist! ❗❗❗")
-            exit()
+        if sheet is not None:
+            if os.path.exists(sheet):
+                continue
+            else:
+                print(f"\n❗❗❗ The file {sheet} does not exist! ❗❗❗")
+                exit()
 
     data_type = os.path.basename(database).split("_")[0]
-    index_col = data_type + "_id"
+    
+    index_col_dict = {"PublicationView": "Pubmed Id",
+                      "DatasetView": "Dataset Alias",
+                      "ToolView": "ToolView_id",
+                      "EducationalResource": "EducationalResource_id"}
+    
+    index_col = index_col_dict.get(data_type)
 
     database_df = pd.read_csv(database, keep_default_na=False, index_col=False)
     print(f"\nDatabase read successfully!")
-    new_entries_df = pd.read_csv(new_entries, keep_default_na=False, index_col=False)
-    print(f"\nNew_entries read successfully!")
+    
+    if new_entries is not None:
+        new_entries_df = pd.read_csv(new_entries, keep_default_na=False, index_col=False)
+        print(f"\nNew_entries read successfully!")
+        filtered_entries_df = filter_updated_manifest(new_entries_df, index_col, data_type)
+        filtered_entries_df.drop(["entityId", "iconTags", "Source"], axis=1, errors="ignore", inplace=True)
+        updated_database = update_database(database_df, filtered_entries_df, index_col)
+        print(f"\nDatabase has been successfully updated!")
+    else:
+        print(f"\nCurrent database will not be updated with corrected entries.")
+        updated_database = database_df
 
-    updated_database = update_database(database_df, new_entries_df, index_col)
-    print(f"\nDatabase has been successfully updated!")
-
-    if data_type in ["PublicationView", "DatasetView"]:
-        updated_database = fill_empty_cells(updated_database, data_type)
-        print(f"\nEmpty cells filled successfully!")
+    updated_database = fill_empty_cells(updated_database, data_type)
+    print(f"\nEmpty cells filled successfully!")
+    updated_database = trim_whitespace(updated_database)
+    print(f"\nWhitespace removed from list entries!")
+    
+    if data_type == "PublicationView":
+        updated_database = fix_pub_doi(updated_database)
 
     updated_database_path = f"{os.getcwd()}/{data_type}_merged_corrected.csv"
     updated_database.to_csv(path_or_buf=updated_database_path, index=False)
