d0762aed841e4be47439990a8112dd392c425426
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-09-05 16:42:37 -0700

    Update csv_to_ttl.py
    - update typing and docstrings
    - allow base tag to be defined at input; use base tag to construct other RDF tags that are not external references

--- utils/csv_to_ttl.py
+++ utils/csv_to_ttl.py
@@ -44,11 +44,19 @@ def get_args():
         required=False,
 		default="new_org"
     )
+	parser.add_argument(
+        "-b",
+		"--base_tag",
+        type=str,
+        help="url applied to the beginning of internal tags",
+        required=False,
+		default="http://syn.org"
+    )
 	return parser.parse_args()
 
 
-def convert_schematic_model_to_ttl_format(input_df: pd.DataFrame, org_name) -> pd.DataFrame:
-
+def convert_schematic_model_to_ttl_format(input_df: pd.DataFrame, org_name: str, base_tag: str) -> pd.DataFrame:
+	"""Convert schematic model DataFrame to TTL format."""
 	out_df = pd.DataFrame()
 	
 	# Step 1: Identify all node rows (treat rows with non-empty DependsOn as nodes)
@@ -63,13 +71,13 @@ def convert_schematic_model_to_ttl_format(input_df: pd.DataFrame, org_name) -> p
 
 	# Step 2: Assign node URI for each attribute
 	out_df["Resolved_Node_URI"] = out_df["Resolved_Node"].apply(
-		lambda x: f"<http://syn.org/mc2/{x.strip().lower().replace(' ', '_')}>"
+		lambda x: f"<{base_tag}/{org_name}/{x.strip().lower().replace(' ', '_')}>"
 		)
 	
 	# Step 3: Construct term URIs for each attribute
-	out_df["term"] = out_df.apply(lambda row: format_uri(row["Resolved_Node"], row["label"], org_name), axis=1)
+	out_df["term"] = out_df.apply(lambda row: format_uri(base_tag, row["Resolved_Node"], row["label"], org_name), axis=1)
 	
-	# Step 4: TTL-compatible column formatting
+	# Step 4: Info extraction and TTL-compatible column formatting
 	for _, row in out_df.iterrows():
 		out_df.at[_, "description"] = attribute_rows.loc[row["label"], "Description"]
 		out_df.at[_, "is_cde"] = get_cde_id(str(attribute_rows.loc[row["label"], "Properties"]))
@@ -89,16 +97,18 @@ def convert_schematic_model_to_ttl_format(input_df: pd.DataFrame, org_name) -> p
 	return out_df[final_cols]
 
 
-def format_uri(node, attribute, org_name):
-	
+def format_uri(base_tag:str, node:str, attribute:str, org_name:str) -> str:
+	"""Format the URI for a given node and attribute."""
+
 	node_segment = node.strip().lower().replace(" ", "_")
 	attr_segment = attribute.strip().lower().replace(" ", "_")
     
-	return f"<http://syn.org/{org_name}/{node_segment}/{attr_segment}>"
+	return f"<{base_tag}/{org_name}/{node_segment}/{attr_segment}>"
 
 
-def convert_schematic_column_type(type, is_enum):
-	
+def convert_schematic_column_type(type:str, is_enum:bool) -> str: 
+	"""Convert schematic column type to TTL-compatible format."""
+
 	if type in ["string", "string_list"]:
 		string_type = "string;enum" if is_enum else "string"
 		if type == "string_list":
@@ -111,8 +121,8 @@ def convert_schematic_column_type(type, is_enum):
 	return out_type
 
 
-def get_cde_id(entry):
-
+def get_cde_id(entry: str) -> str:
+	"""Extract CDE ID from Properties entry."""
 	entry = entry.split(", ") if len(entry.split(", ")) > 1 else entry
 	
 	if type(entry) == list:
@@ -126,14 +136,14 @@ def main():
 	
 	args = get_args()
 
-	base_tag = "<http://syn.org>"
+	base_tag = args.base_tag
 	label_tag = "<http://www.w3.org/2000/01/rdf-schema#label>"
 	desc_tag = "<http://purl.org/dc/terms/description>"
-	node_tag = "<http://syn.org/node>"
-	type_tag = "<http://syn.org/type>"
-	req_tag = "<http://syn.org/requiredBy>"
-	cde_tag = "<http://syn.org/isCDE>"
-	key_tag = "<http://syn.org/isKey>"
+	node_tag = f"<{base_tag}/node>"
+	type_tag = f"<{base_tag}/type>"
+	req_tag = f"<{base_tag}/requiredBy>"
+	cde_tag = f"<{base_tag}/isCDE>"
+	key_tag = f"<{base_tag}/isKey>"
 
 	if args.mapping:
 		print(f"Processing RDF triples precursor CSV [{args.mapping}]...")
@@ -143,7 +153,7 @@ def main():
 	elif args.model:
 		print(f"Processing model [{args.model}] to RDF triples precursor CSV...")
 		model_df = pd.read_csv(args.model, header=0, keep_default_na=True)
-		ttl_df = convert_schematic_model_to_ttl_format(model_df, args.org_name)
+		ttl_df = convert_schematic_model_to_ttl_format(model_df, args.org_name, base_tag)
 		print(f"RDF triples will be built from the generated precursor dataframe!")
 
 	out_file = "/".join([args.output, f"{args.org_name}.ttl"])
