efdd19819139e0389378d0d3abc0bcb1f559afa1
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-09-10 15:26:17 -0700

    Create build_ttl_graphs.sh
    #Usage: bash build_ttl_graphs.sh [SOURCE_DIR] [FILE_EXTENSION] [ORG_NAME]
    
    This script will convert files of the given extension to TTL-formatted RDF via csv_to_ttl.py
    It will concatenate multiple TTLs to a single file, if multiple were provided in the source directory

---
+++ build_ttl_graphs.sh
@@ -0,0 +1,28 @@
+#/usr/bin/bash
+
+#Usage: bash build_ttl_graphs.sh [SOURCE_DIR] [FILE_EXTENSION] [ORG_NAME] [VERSION]
+
+#This script will convert files of the given extension to TTL-formatted RDF via csv_to_ttl.py
+#It will concatenate multiple TTLs to a single file, if multiple were provided in the source directory
+
+
+dir="$1"
+datatype="$2"
+org="$3"
+version="$4"
+outdir="ttl_graphs"
+
+mkdir -p ./"$outdir"
+
+for file in "$dir"/*."$datatype"; do
+	if [ -f "$file" ]; then
+		python utils/csv_to_ttl.py -m "$file" -g "$org" -o "$outdir" -v "$version"
+	fi
+done
+
+for ttl in "$outdir"/*.ttl; do
+	if [ -f "$ttl" ]; then
+		cat "$ttl" >> "$org"_all.ttl
+		mv "$org"_all.ttl "$outdir"
+	fi
+done
