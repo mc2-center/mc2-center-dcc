a03da9660192410a6a3f48cee92cb155a9fc3e4e
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-09-25 17:00:39 -0700

    Create add_json_conditions.py
    Add script that modifies JSON schemas from schematic to include a "contains" label for attributes with enums in conditionals. This is to allow multiple conditions to be applied, based on a single input array

---
+++ utils/add_json_conditions.py
@@ -0,0 +1,35 @@
+import json
+import argparse
+import pathlib
+
+
+def generate_json_schema(input):
+    """
+    Corrects conditional logic for arrays.
+    """
+
+    output = "".join([pathlib.Path(input).stem, ".updated.json"]) 
+    
+    with open(input, 'r') as i:
+        model = json.load(i)
+
+    conditions = model["allOf"]
+
+    for property in conditions:
+        for prop in property["if"]["properties"]:
+            if property["if"]["properties"][prop]["enum"]:
+                property["if"]["properties"][prop] = { "contains": property["if"]["properties"][prop]}
+    
+    with open(output, 'w') as f:
+        json.dump(model, f, indent=2)
+
+    print(f"✅ JSON Schema written to {output}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate corrected JSON schema")
+    parser.add_argument("input_path", help="Path to a JSON schema file.")
+    args = parser.parse_args()
+
+    
+    generate_json_schema(args.input_path)
