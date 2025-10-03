c3d2ef428fb4773cd9b239bdb957ef1c0ec5a3e0
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-10-03 15:58:18 -0700

    Refactor to use prefixes

--- utils/build_template_ttl.py
+++ utils/build_template_ttl.py
@@ -55,12 +55,20 @@ def get_args():
 		default="new_org"
     )
 	parser.add_argument(
-        "-p",
-		"--tag_prefix",
+        "-b",
+		"--base_tag",
         type=str,
         help="The tag that will be used as a prefix in RDF",
         required=False,
 		default="http://syn.org"
+    )
+	parser.add_argument(
+        "-r",
+		"--base_ref",
+        type=str,
+        help="The prefix that will be used to represent the base_tag",
+        required=False,
+		default="syn"
     )
 	parser.add_argument(
         "-v",
@@ -82,10 +90,36 @@ def main():
 	
 	args = get_args()
 
-	prefix_tag = args.tag_prefix
-	conform_tag = "<http://purl.org/dc/terms/conformsTo>"
-	int_tag = "<http://www.w3.org/2001/XMLSchema#integer>"
+	base_tag = args.base_tag
+	base_ref = args.base_ref
+
+	org_tag = args.org_name
+	conform_tag = "conformsTo"
+	int_tag = "integer"
 	version = args.version
+	template_tag = "Template"
+	col_tag = "hasColumn"
+	pos_tag = "ColumnPosition"
+	colname_tag = "column"
+	header_tag = "header"
+	value_tag = "position"
+	def_val_tag = "defaultValue"
+	uuid_tag = "uuid"
+
+	tag_dict = {  # Can replace tuples with alternative tag definitions
+		base_ref : (base_ref, f"<{base_tag}/>"),
+		org_tag : (org_tag, f"<{base_ref}:{org_tag}/>"),
+		conform_tag : ("purl", "<http://purl.org/dc/terms/>"),
+		int_tag : ("xml", "<http://www.w3.org/2001/XMLSchema#>"),
+		template_tag : (base_ref, f"<{base_tag}/>"),
+		col_tag : (base_ref, f"<{base_tag}/>"),
+		uuid_tag : (base_ref, f"<{base_tag}/>"),
+		pos_tag : (base_ref, f"<{base_tag}/>"),
+		colname_tag : (base_ref, f"<{base_tag}/>"),
+		header_tag : (base_ref, f"<{base_tag}/>"),
+		value_tag : (base_ref, f"<{base_tag}/>"),
+		def_val_tag: (base_ref, f"<{base_tag}/>")
+		}
 	
 	if args.template:
 		print(f"Processing model [{args.template}] to template.ttl...")
@@ -95,17 +129,24 @@ def main():
 		template_df = pd.read_csv(args.template, header=0, keep_default_na=True, sep=sep)
 
 	if template_name.startswith("GC_Data_Loading_Template"):
-		template_name = template_name.split("_")[-2]
-		version = template_name.split("_")[-1]
+		template_name_split = template_name.split("_")
+		template_name = template_name_split[-2]
+		version = template_name_split[-1]
 
 	out_file = "/".join([args.output, f"{args.org_name}_{template_name}_{version}.ttl"])
 
 	with open(out_file, "w+") as f:
 		print(f"Building RDF triples and serializing to TTL...")
-
+		prefix_set = [prefix for prefix in tag_dict.keys()]
+		first_lines = [f"@prefix {tag_dict[prefix][0]}: {tag_dict[prefix][1]}"+" .\n" for prefix in prefix_set]
+		template_name_tag = f"@prefix {template_name}: <{tag_dict[template_tag][0]}:{template_name}/>"+" .\n\n"
+		first_lines_set = "".join(sorted(set(first_lines)))
+		f.write(first_lines_set)
+		f.write(template_name_tag)
+		
 		# write template definition
-		f.write(f"<{prefix_tag}/{args.org_name}/{template_name}> a <{prefix_tag}/Template> ."+"\n")
-		f.write(f"<{prefix_tag}/{args.org_name}/{template_name}> {conform_tag} <{prefix_tag}/{args.org_name}> ."+"\n")
+		f.write(f"{tag_dict[org_tag][0]}:{template_name} a {tag_dict[base_ref][0]}:{template_tag} ."+"\n")
+		f.write(f"{tag_dict[template_tag][0]}:{template_name} {tag_dict[conform_tag][0]}:{conform_tag} {tag_dict[base_ref][0]}:{org_tag} ."+"\n")
 		
 		# write column definitions
 		# set col position counter
@@ -114,13 +155,13 @@ def main():
 			clean_col = format_uri(col)
 			col_uuid = uuid4()
 			if col in ["Component", "type"]:
-				f.write(f'<{prefix_tag}/{args.org_name}/{template_name}/{clean_col}> <{prefix_tag}/defaultValue> "{template_name}" .'+"\n")
-			f.write(f"<{prefix_tag}/{args.org_name}/{template_name}> <{prefix_tag}/hasColumn> <{prefix_tag}/{args.org_name}/{template_name}/{clean_col}> ."+"\n")
-			f.write(f"<{prefix_tag}/{col_uuid}>a <{prefix_tag}/ColumnPosition> ;"+"\n")
-			f.write(f"<{prefix_tag}/template> <{prefix_tag}/{args.org_name}/{template_name}> ;"+"\n")
-			f.write("\t"+f"<{prefix_tag}/column> <{prefix_tag}/{args.org_name}/{template_name}/{clean_col}> ;"+"\n")
-			f.write("\t"+f'<{prefix_tag}/header> "{col}" ;'+"\n")
-			f.write("\t"+f'<{prefix_tag}/position> "{col_position}"^^{int_tag} .'+"\n")
+				f.write(f'{template_name}:{clean_col} {tag_dict[def_val_tag][0]}:{def_val_tag} "{template_name}" .'+"\n")
+			f.write(f"{tag_dict[template_tag][0]}:{template_name} {tag_dict[col_tag][0]}:{col_tag} {template_name}:{clean_col} ."+"\n")
+			f.write(f"{tag_dict[uuid_tag][0]}:{col_uuid} a {tag_dict[pos_tag][0]}:{pos_tag} ;"+"\n")
+			f.write(f"{tag_dict[template_tag][0]}:{template_tag} {tag_dict[org_tag][0]}:{template_name} ;"+"\n")
+			f.write("\t"+f"{tag_dict[colname_tag][0]}:{colname_tag} {template_name}:{clean_col} ;"+"\n")
+			f.write("\t"+f'{tag_dict[header_tag][0]}:{header_tag} "{clean_col}" ;'+"\n")
+			f.write("\t"+f'{tag_dict[value_tag][0]}:{value_tag} "{col_position}"^^{tag_dict[int_tag][0]}:{int_tag} .'+"\n")
 			col_position += 1
 	
 	print(f"Done ✅")
