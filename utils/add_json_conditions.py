3828493d2ce52058114e3944a9f3bc7bf78b756f
Orion Banks <49208907+Bankso@users.noreply.github.com>
2025-10-21 14:43:01 -0700

    Add ref to AR-related schema if passed

--- utils/add_json_conditions.py
+++ utils/add_json_conditions.py
@@ -3,7 +3,7 @@ import argparse
 import pathlib
 
 
-def generate_json_schema(input):
+def generate_json_schema(input, ar_schema):
     """
     Corrects conditional logic for arrays.
     """
@@ -20,6 +20,10 @@ def generate_json_schema(input):
             if property["if"]["properties"][prop]["enum"]:
                 property["if"]["properties"][prop] = {"contains": property["if"]["properties"][prop]}
     
+    ref_conditions = {"$ref": f"{ar_schema}"} if ar_schema is not None else None
+
+    conditions = conditions.append(ref_conditions)
+
     with open(output, 'w') as f:
         json.dump(model, f, indent=2)
 
@@ -29,7 +33,8 @@ def generate_json_schema(input):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate corrected JSON schema")
     parser.add_argument("input_path", help="Path to a JSON schema file.")
+    parser.add_argument("ar_schema", help="URI for AR JSON schema")
     args = parser.parse_args()
 
     
-    generate_json_schema(args.input_path)
+    generate_json_schema(args.input_path, args.ar_schema)
