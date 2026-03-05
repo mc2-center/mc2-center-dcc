c2e741aad2e6072bf69c5edd087edb1d2edb70d3
Orion Banks <49208907+Bankso@users.noreply.github.com>
2026-03-04 16:37:28 -0800

    Allow RecordSets to be used as source for UNION tables
    Added a function to identify record sets of a given data type, pull the content, create a new Synapse table for each record set in its source Synapse project, then create a materialized view for all the new tables. This can be configured to recapitulate the structure, schemas, etc. of existing synapse_storage_manifest_tables, so that Curator and RecordSets can be easily integrated into the CCKP database as needed

--- portal_tables/merge_tables.py
+++ portal_tables/merge_tables.py
@@ -17,8 +17,10 @@ author: orion.banks
 
 import synapseclient
 from synapseclient import MaterializedViewSchema
+from synapseclient.models import RecordSet, SchemaStorageStrategy, Table
 import argparse
 import pandas as pd
+import re
 
 
 ### Login to Synapse ###
@@ -46,6 +48,13 @@ def get_args():
         type=str,
         choices=["PublicationView", "DatasetView", "ToolView", "EducationalResource"],
         help="Name of metadata component being merged into table.",
+    )
+    parser.add_argument(
+        "-rec",
+        "--target_record_set",
+        action="store_true",
+        default=None,
+        help="Boolean. If provided, the program will search for Synapse RecordSets of the requested data type in MC2 Center projects"
     )
     return parser.parse_args()
 
@@ -77,31 +86,62 @@ def build_query(table_ids):
     return full_query
 
 
+def get_record_sets(syn, record_type, folder_name, source = "syn21918972", org = "MC2Center"):
+    
+    project_ids = syn.tableQuery(f"SELECT 'grantId' FROM {source}")
+    
+    record_df = pd.DataFrame(data=["syn71723047"], columns=["grantId"]) #pd.DataFrame(project_ids)
+    record_df["recordId"] = ""
+    record_name = "_".join([org, record_type])
+
+    for _,row in record_df.iterrows():
+        folder_id = syn.findEntityId(name=folder_name, parent=row["grantId"])
+        record_id = syn.findEntityId(name=record_name, parent=folder_id)
+        record_set = RecordSet(id=record_id).get()
+        record_set_df = pd.read_csv(record_set.path, header=0)
+        #apply sorting, column naming, extraction, etc.
+        record_set_table = Table(name=f"{record_name}_table", parent_id=row["grantId"]).store()
+        record_set_table.store_rows(values=record_set_df, schema_storage_strategy=SchemaStorageStrategy.INFER_FROM_DATA)
+        record_df.at[_,"recordId"] = record_set_table.id
+    
+    return record_df
+
 def main():
 
     syn = login()
     args = get_args()
-    source, target, table_type = args.s, args.t, args.n
+    source, target, table_type, record_sets = args.s, args.t, args.n, args.target_record_set
+
+    folder_map = {
+        "PublicationView" : "publications",
+        "DatasetView" : "datasets",
+        "ToolView" : "tools",
+        "EducationalResource" : "education"
+    }
 
     if table_type in [
         "PublicationView",
         "DatasetView",
         "ToolView",
-        "EducationalResource",
+        "EducationalResource"
     ]:
 
         label = f"{table_type}_UNION"
-
-        form_table_type = table_type.lower()
-
-        view_type = f"{form_table_type}_synapse_storage_manifest_table"
+        view_type = f"{table_type.lower()}_synapse_storage_manifest_table"
+        record_type =  f"{table_type}_RecordSet"
+        folder_name = folder_map[table_type]
 
     else:
         print(
             f"{table_type} is not a valid table type. Please select a different component."
         )
-
-    table_ids_from_view = get_table_ids(syn, source, view_type, "id")
+    
+    if record_sets is not None:
+        record_df = get_record_sets(syn, record_type, folder_name)
+        table_ids_from_view = record_df["recordId"].to_list()
+    else:
+        table_ids_from_view = get_table_ids(syn, source, view_type, "id")
+    
     print(table_ids_from_view)
 
     table_query = build_query(table_ids_from_view)
